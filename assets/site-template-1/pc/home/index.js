9b10108bc107d5244eb6f5aeab9d3126ecb7307e
DJ <dj@Macintosh.local>
2020-11-17 18:48:23 +0800

    [feat]底部和联系我们组件

--- assets/site-template-1/pc/home/index.js
+++ assets/site-template-1/pc/home/index.js
@@ -7,6 +7,7 @@ var swiper = new Swiper('.swiper-container', {
   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
+    waitForTransition: true
   },
   pagination: {
     el: '.swiper-pagination',
