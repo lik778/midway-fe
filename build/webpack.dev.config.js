92c7a825655a8058664e4c7a5d403df0b138ab1c
wulei <wulei@baixing.com>
2020-11-18 10:41:44 +0800

    feat: 1.拆分webpack打包  1.添加了prod前端打包。

--- build/webpack.dev.config.js
+++ build/webpack.dev.config.js
@@ -1,47 +1,10 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { merge } = require('webpack-merge');
+const baseConfig = require('./webpack.base.config');
 
-module.exports = {
+const devConfig = {
   watch: true,
   devtool: 'source-map',
-  mode: 'development',
-  entry: {
-    'site-template-1-home-pc': path.resolve(__dirname, '..', 'assets/site-template-1/pc/home/index.js'),
-    'site-template-1-home-wap': path.resolve(__dirname, '..', 'assets/site-template-1/wap/home/index.js'),
-    'site-template-1-news-pc': path.resolve(__dirname, '..', 'assets/site-template-1/pc/news/index.js'),
-    'site-template-1-news-wap': path.resolve(__dirname, '..', 'assets/site-template-1/wap/news/index.js'),
-    'site-template-1-product-pc': path.resolve(__dirname, '..', 'assets/site-template-1/pc/product/index.js'),
-    'site-template-1-product-wap': path.resolve(__dirname, '..', 'assets/site-template-1/wap/product/index.js'),
-  },
-  output: {
-    path: path.resolve(__dirname, "../dist/public"),
-  },
-  plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.styl(us)?$/,
-        use: [
-          {
-            loader: MiniCssExtractPlugin.loader,
-            options: {
-              esModule: false
-            },
-          }, 'css-loader', 'stylus-loader']
-      },
-    ]
-  },
-
+  mode: 'development'
 }
+
+module.exports = merge(devConfig, baseConfig)
