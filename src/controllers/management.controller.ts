2d09c6d07c5e0ed39816f66d10af48d728078ad0
wulei <wulei@baixing.com>
2020-11-19 09:46:27 +0800

    chore: 添加了guard组件

--- src/controllers/management.controller.ts
+++ src/controllers/management.controller.ts
@@ -1,13 +1,20 @@
-import { Controller, Post, Body, Req, Res } from '@nestjs/common';
+import { Controller, UseGuards, Post, Get, Body, Req, Res } from '@nestjs/common';
 import { ApiQeqDTO } from '../dto/api-req.dto';
+import  { VipUserGuard } from '../guards/vip-user.guard'
 import { Request, Response } from 'express';
 
 
 @Controller('/management')
 export class ManagementController {
 
+  @Get('/')
+  @UseGuards(VipUserGuard)
+  async managementView(@Req() req: Request, @Res() res: Response) {
+    res.send('view enter')
+  }
+
   @Post('/api')
-  async getBackStageData(@Req() req: Request, @Res() res: Response, @Body() body: ApiQeqDTO) {
-    res.json({ name: 'wulei' })
+  async managementApi(@Req() req: Request, @Res() res: Response, @Body() body: ApiQeqDTO) {
+    res.json({ name: 'api' })
   }
 }
