1002bb2914cfd000f8e3ec7fa998135ddf3b4650
ChenPing <chenping@baixing.com>
2020-11-25 10:22:48 +0800

    增加店铺管理ui

---
+++ app/mvip-manager/src/components/shop-box/index.tsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import './index.less';
+export default (props: any) => {
+  return (
+    <div className="shop-box">
+      <div className="s-img">
+        <img src="//file.baixing.net/202011/7f14b8c736a614c75fda50d1d1615640.png"/>
+        <div className="s-status">生效中</div>
+      </div>
+      <h4>店铺名店铺名店铺名店铺名店铺名店铺名店铺名店铺名店铺名
+        <span className="s-edit iconfont">&#xe61b;</span>
+      </h4>
+      <div className="s-btn">
+        <a href="">基础设置</a>
+        <a href="">内容管理</a>
+      </div>
+    </div>
+  );
+}
