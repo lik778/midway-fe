57e3e62dfc16a15319460da282fc29a26a2d8a4b
wulei <wulei@baixing.com>
2020-11-26 09:55:10 +0800

    feat: UserAgent装饰器添加了device字段

--- src/controllers/site.controller.ts
+++ src/controllers/site.controller.ts
@@ -8,48 +8,45 @@ export class SiteController {
   constructor(private midwayApiService: MidwayService) {}
 
   @Get('/')
-  public async home(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('isWap') isWap) {
+  public async home(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('device') device) {
       const shopName = this.midwayApiService.getShopName(params.shopName)
-      // tips: 这里要重新写装饰器
-      const device = isWap ? 'wap' : 'pc'
       const templateUrl = `site-template-1/${device}/home/index`
       const { data } = await this.midwayApiService.getHomePageData(shopName, device);
       return res.render(templateUrl, { title: '首页', renderData: { ...data, shopName }, isHome: true });
   }
 
   @Get('/n')
-  async listing(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('isWap') isWap) {
+  async listing(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('device') device) {
     const shopName = this.midwayApiService.getShopName(params.shopName)
-    const device = isWap ? 'wap' : 'pc'
     const { data } = await this.midwayApiService.getProductPageData(shopName, device, { page: 1, size: 0 });
-    const templateUrl = `site-template-1/${isWap ? 'wap' : 'pc'}/news/index`
+    const templateUrl = `site-template-1/${device}/news/index`
     return res.render(templateUrl, { title: '新闻资讯', renderData: { ...data, shopName } });
   }
 
   @Get('/n-:id')
-  async newschild(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('isWap') isWap) {
+  async newschild(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('device') device) {
     if (/.html$/.test(req.url)) {
-      const templateUrl = `site-template-1/${isWap ? 'wap' : 'pc'}/news-detail/index`
+      const templateUrl = `site-template-1/${device}/news-detail/index`
       return res.render(templateUrl, { title: '资讯详情' });
     } else {
-      const templateUrl = `site-template-1/${isWap ? 'wap' : 'pc'}/news-child/index`
+      const templateUrl = `site-template-1/${device}/news-child/index`
       return res.render(templateUrl, { title: '资讯子类' });
     }
   }
 
   @Get('/p')
-  async product(@Req() req: Request, @Res() res: Response, @UserAgent('isWap') isWap) {
-    const templateUrl = `site-template-1/${isWap ? 'wap' : 'pc'}/product/index`
+  async product(@Req() req: Request, @Res() res: Response, @UserAgent('device') device) {
+    const templateUrl = `site-template-1/${device}/product/index`
     return res.render(templateUrl, { title: '产品服务' });
   }
 
   @Get('/p-:id')
-  async productchild(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('isWap') isWap) {
+  async productchild(@Param() params, @Req() req: Request, @Res() res: Response, @UserAgent('device') device) {
     if (/.html$/.test(req.url)) {
-      const templateUrl = `site-template-1/${isWap ? 'wap' : 'pc'}/product-detail/index`
+      const templateUrl = `site-template-1/${device}/product-detail/index`
       return res.render(templateUrl, { title: '产品详情页' });
     } else {
-      const templateUrl = `site-template-1/${isWap ? 'wap' : 'pc'}/product-child/index`
+      const templateUrl = `site-template-1/${device}/product-child/index`
       return res.render(templateUrl, { title: '服务子类' });
     }
   }
