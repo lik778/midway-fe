faecb13101bcd8b3899f64aafc9c879fe0a79a11
wulei <wulei@baixing.com>
2020-12-01 11:07:40 +0800

    feat: 完成了shopModule组件的渲染逻辑，并新增枚举

--- app/mvip-manager/src/components/shop-module/index.tsx
+++ app/mvip-manager/src/components/shop-module/index.tsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { ReactComponentElement, useState } from 'react';
 import MainTitle from '@/components/main-title';
 import ArticleBox from '@/components/article-box';
+import ModuleList from '@/components/shop-module-list';
+import ModuleEmpty from '@/components/shop-module-empty';
 import { Menu } from 'antd';
 import { Link } from 'umi';
 import './index.less'
+import { ShopModuleType } from '@/enums';
 
 const menuList = [
   {
@@ -17,12 +20,28 @@ const menuList = [
     key: 'article',
   }
 ]
-export default (props: any) => {
-  const path = props.location.pathname.split('/')
-  const [current, setCurrent] = useState(path[2])
+
+interface Props {
+  type: ShopModuleType;
+}
+
+export default (props: Props) => {
+  const [current, setCurrent] = useState(props.type)
   const handleClick = (e: { key: any; }) => {
     setCurrent(e.key)
   };
+  const hasData = false;
+  let containerComponent;
+  if (hasData) {
+    containerComponent = (
+      <div className="container">
+        <ArticleBox/>
+        <ModuleList/>
+      </div>
+    )
+  } else {
+    containerComponent = <ModuleEmpty type={props.type}/>
+  }
 
   return (
     <div>
@@ -35,7 +54,7 @@ export default (props: any) => {
         })}
       </Menu>
       <div className="container">
-        <ArticleBox />
+        { containerComponent }
       </div>
     </div>
   );
