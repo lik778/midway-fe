bdbdb4e6818a416243711a77b880536582f92dc0
DJ <dj@Macintosh.local>
2020-12-02 21:18:13 +0800

    fix PC端最后的各种细节和数据

--- assets/site-template-1/pc/components/customer-service/index.js
+++ assets/site-template-1/pc/components/customer-service/index.js
@@ -4,9 +4,11 @@ export const qqModule = function() {
 	$(document).ready(function(){
 		$(".qq-title").click(function(){
 			$(".customer-box").hide();
+			$(".customer-hide").show();
 		});
 		$(".customer-hide").click(function(){
 			$(".customer-box").show();
+			$(".customer-hide").hide();
 		});
 	});
 }
