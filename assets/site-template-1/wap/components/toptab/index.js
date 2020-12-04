0ef72d93b78a8c707efc7223caff529e860ce47f
DJ <dj@Macintosh.local>
2020-12-04 23:44:41 +0800

    [feat]产品列表修复问题

--- assets/site-template-1/wap/components/toptab/index.js
+++ assets/site-template-1/wap/components/toptab/index.js
@@ -1,13 +1,11 @@
 import $ from 'jquery'
 
-export const aboutModule = function(){
-	//等待DOM 结构加载完成，然后再执行操作
+export const tabModule = function(){
 	$(document).ready(function(){
 		$(".more-about").click(function(){
-			$(".aboutus-content>p").toggleClass("about-hidden");
+			$(".tab-header>ul").toggleClass("tab-hidden");
 			$(".more-about>img").toggleClass("img-rotate");
 			$(".more-about").toggleClass("change-more-about")
 		})
 	})
 }
-
