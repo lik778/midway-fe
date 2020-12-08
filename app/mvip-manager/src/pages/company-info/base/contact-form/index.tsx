073b39640866f7ba6e7efcb09b1bd77b3f940882
wulei <wulei@baixing.com>
2020-12-08 20:46:20 +0800

    feat: 完成了基础资料和联系方式数据对接。(这边地区组件还要改一下)

--- app/mvip-manager/src/pages/company-info/base/contact-form/index.tsx
+++ app/mvip-manager/src/pages/company-info/base/contact-form/index.tsx
@@ -1,34 +1,57 @@
-import React from 'react'
-import { Button, Form } from 'antd';
-import { PlusOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Button, Form, message } from 'antd';
 import WildcatForm from '@/components/wildcat-form';
 import { contactForm } from '@/config/form';
 import { QQCustomService } from '@/pages/company-info/base/qq-custom-service';
+import { QQItem, UserEnterpriseInfo } from '@/interfaces/user';
+import { FormConfig } from '@/components/wildcat-form/interfaces';
+import { saveEnterpriseContactInfoApi } from '@/api/user'
 
-// const styles = {
-//   addVendor: { width: 120, fontSize: 14, color: '#096DD9' }
-// }
 interface Props {
+  editDataSource: UserEnterpriseInfo | null;
   back(): void;
 }
 
 const ContactForm = (props: Props) => {
-  const sumbit = (values: any) => {
-    console.log(values)
+  const { editDataSource } = props
+  const [qqList, setQQList] = useState<QQItem[]>([]);
+  const [config, setConfig] = useState<FormConfig>(contactForm);
+  const [formData, setFormData] = useState<any>(null);
+
+  const formChange = (changeValue: any, allValues: any) => {
+    setFormData(allValues);
   }
+
+  // 这里前置要校验一下
+  const saveInfo = async() => {
+    let qqMap: any = null
+    if (qqList.length > 0) {
+      qqMap = {}
+      qqList.forEach(x => qqMap[x.qq] = x.name)
+    }
+    const info = Object.assign(editDataSource, formData)
+    info.qqMap = qqMap
+    const res = await saveEnterpriseContactInfoApi(info)
+    if (res.success) {
+       message.success('更新联系方式成功')
+    } else {
+      message.error(res.message)
+    }
+  }
+
   return (
     <div>
       <Form.Item label="电话/微信">
-        <WildcatForm config={contactForm} submit={sumbit}/>
+        <WildcatForm editDataSource={editDataSource} config={config} formChange={formChange}/>
       </Form.Item>
       {/*<Form.Item className={styles.item} style={{ margin: '33px 0' }}>*/}
       {/*  <label className={styles.label}>第三方客服</label>*/}
       {/*  <Button className={styles.addVendor} type="dashed" size="large" icon={<PlusOutlined />} block>添加代码</Button>*/}
       {/*</Form.Item>*/}
       <Form.Item label="QQ客服">
-        <QQCustomService />
+        <QQCustomService editDataSource={editDataSource} onChange={(qqList) => setQQList(qqList)}/>
       </Form.Item>
-      <Button type="primary" size="large">保存</Button>
+      <Button type="primary" size="large" onClick={saveInfo}>保存</Button>
       <Button onClick={props.back} style={{ margin: '0 8px' }} size="large">上一步</Button>
     </div>
   )
