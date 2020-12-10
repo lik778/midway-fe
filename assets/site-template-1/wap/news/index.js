4b1a81e83828507e4b3abf39ebfcd81b3929a94e
DJ <dj@Macintosh.local>
2020-12-10 17:29:11 +0800

    [fix]分页修改

--- assets/site-template-1/wap/news/index.js
+++ assets/site-template-1/wap/news/index.js
@@ -1,5 +1,8 @@
 import './index.styl'
 import { menusModule } from '../components/header/index'
 import { tabModule } from '../components/toptab/index'
+//import { s_click } from '../components/pagination/index'
+
 menusModule()
 tabModule()
+//s_click()
