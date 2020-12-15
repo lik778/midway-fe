c4a28ef1f84af2a3b627987661d65dd3e916fa37
ChenPing <chenping@baixing.com>
2020-12-15 11:34:18 +0800

    跳转有误

--- app/mvip-manager/src/components/shop-box/index.tsx
+++ app/mvip-manager/src/components/shop-box/index.tsx
@@ -24,8 +24,8 @@ export default (props: any) => {
         <img src={s?.thumb}/>
         <div className={`s-status ${status}`}>{statusTxt}</div>
       </div>
-      <div className="s-title" onClick={handleClick}>
-        <h4>{s?.name}</h4>
+      <div className="s-title">
+        <h4 onClick={handleClick}>{s?.name}</h4>
         <span className="s-edit iconfont" onClick={props.onClick}>&#xe61b;</span>
       </div>
       <div className="s-btn">
