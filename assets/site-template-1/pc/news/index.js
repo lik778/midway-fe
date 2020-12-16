9cdfd1dcd7dee99bbee5ed1ef986317ec8e1f51e
DJ <dj@Macintosh.local>
2020-12-16 20:18:04 +0800

    [wap]端产品详情页增加图片

--- assets/site-template-1/pc/news/index.js
+++ assets/site-template-1/pc/news/index.js
@@ -1,5 +1,6 @@
 import './index.styl'
 import { qqModule } from '../components/customer-service/index';
 import { navModule } from '../components/nav/index';
+
 navModule()
 qqModule()
--- assets/site-template-1/pc/product-child/index.js
+++ assets/site-template-1/pc/product-child/index.js
@@ -1,5 +1,8 @@
 import './index.styl'
 import { qqModule } from '../components/customer-service/index';
 import { navModule } from '../components/nav/index';
+import { categoryModule } from '../components/sidebar/category-item/index'
+
 navModule()
 qqModule()
+categoryModule()
