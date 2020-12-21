ec5dd42458186e7922bb9742442d995572698c78
DJ <dj@Macintosh.local>
2020-12-21 15:13:27 +0800

    [feat]wap端拨打电话

--- assets/site-template-1/wap/components/aboutus/index.js
+++ assets/site-template-1/wap/components/aboutus/index.js
@@ -1,19 +1,22 @@
 import $ from 'jquery'
+import { eventTracker } from '../../../../common/tracker';
 
 export const aboutModule = function(){
 	//等待DOM 结构加载完成，然后再执行操作
 	$(document).ready(function(){
 		$(".s-open").click(function(){
 			$(".aboutus-content>p").removeClass("about-hidden");
-			//$(".s-open>img").toggleClass("img-rotate");
-			//$(".more-about").toggleClass("change-more-about")
 			$(".s-open").hide()
 			$(".s-off").show()
-		})
+		});
 		$(".s-off").click(function(){
 			$(".aboutus-content>p").addClass("about-hidden");
 			$(".s-open").show()
 			$(".s-off").hide()
+		});
+		//对拨打电话打点
+		$(".introduction a").click(()=>{
+			eventTracker("phone-wap", "home-wap")
 		})
 	})
 }
