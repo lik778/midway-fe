0e76d36acefdefb5aadecb8c1d36fbe823480b08
ChenPing <chenping@baixing.com>
2020-12-23 19:49:53 +0800

    修改状态

--- app/mvip-manager/src/pages/shop/index.tsx
+++ app/mvip-manager/src/pages/shop/index.tsx
@@ -32,6 +32,8 @@ export default (props: any) => {
   const [shopOperateStatus, setOperate] = useState(0)
   // 是否是后缀域名(前缀B2B，后缀服务)
   const [isSuffix, setIsSuffix] = useState(true)
+  // 是否是后缀域名(前缀B2B，后缀服务)
+  const [isNewShopDisabled, setIsNewShopDisabled] = useState(false)
   // 弹窗报错
   const [isInputErr, setInputErr] = useState({
     key:'',
@@ -53,8 +55,6 @@ export default (props: any) => {
   })
 
   // 相关文案和参数处理
-  // 限制只显示2个
-  let isNewShopDisabled = false
   // 弹窗文案
   const operateShopTxt = shopOperateStatus === 0 ? '新建店铺' : '修改店铺'
   const isDomainDisabled = shopOperateStatus === 1 ? true : false
@@ -161,7 +161,7 @@ export default (props: any) => {
     if (notEmptyObject(shopStatus)) {
       setEditVisible(!shopStatus.isUserPerfect)
       setIsSuffix(shopStatus.domainType === 'SUFFIX')
-      isNewShopDisabled = !(shopStatus?.isTicketAvailable) || false
+      setIsNewShopDisabled(!(shopStatus?.isTicketAvailable))
     }
   }, [shopStatus])
 
