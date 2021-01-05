93743af4648ab6c55d31f6da4240b71d1a5d6313
wulei <wulei@baixing.com>
2021-01-05 14:19:31 +0800

    chore: 去掉种cookie操作

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -1,9 +1,8 @@
-import { BadRequestException, Controller, Get, HttpException, HttpStatus, Req, Res } from '@nestjs/common';
+import { Controller, Get, HttpException, HttpStatus, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { join } from 'path';
 import * as fs  from 'fs';
-import config, { DEVELOPMENT_ENV, TEST_ENV } from '../config';
-import { COOKIE_HASH_KEY, COOKIE_TOKEN_KEY, COOKIE_USER_KEY } from '../constant/cookie';
+import config from '../config';
 
 const staticFiles = join(__dirname , '..', '..', '/assets/static')
 const files = fs.readdirSync(staticFiles);
@@ -29,21 +28,6 @@ export class AppController {
   @Get('/midway/health')
   async managementView(@Req() req: Request, @Res() res: Response) {
     res.send(process.env.NODE_ENV + ': everything is ok!!!')
-  }
-
-  @Get('/midway/set-cookie')
-  async getCookies(@Req() req: Request, @Res() res: Response) {
-    if ([DEVELOPMENT_ENV, TEST_ENV].includes(config().env)) {
-      [COOKIE_TOKEN_KEY, COOKIE_HASH_KEY, COOKIE_USER_KEY].forEach((cookieKey: string) => {
-        const cookieValue = req.cookies[cookieKey]
-        if (cookieValue) {
-          res.cookie(cookieKey, cookieValue, { httpOnly: true, domain: `localhost` })
-        }
-      })
-      res.json({ success: true, message: '操作成功', code: 200 })
-    } else {
-      throw new BadRequestException('不能进行cookie操作')
-    }
   }
 
 }
