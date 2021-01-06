a35ba89f6ccc4c7bd4926da731059734643323fc
wulei <wulei@baixing.com>
2021-01-06 17:41:54 +0800

    feat: quota区分服务和B2B文案

--- app/mvip-manager/src/components/recharge/index.tsx
+++ app/mvip-manager/src/components/recharge/index.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 import './index.less';
 export default (props: any) => {
   const {quota} = props
-  
+
   const url = ()=> {
     if(quota?.buyUrl){
       return (<a href={quota?.buyUrl}><i className="highlight left-m">去充值&gt;</i></a>)
@@ -12,10 +12,10 @@ export default (props: any) => {
   const quotaPage = ()=> {
     if(quota) {
       return (<div className="recharge">
-        <span>当前免费剩余发文量:  <i className="highlight">{quota?.freeNum || 0}篇</i></span>
-        <span className="left-m">当前剩余信息发布点:  <i className="highlight">{quota?.postRemain || 0}点</i></span>
-        {url()}
-        <span className="left-m">(消耗顺序： 免费发文&gt;信息发布点，1篇文章消耗6个信息发布点)</span>
+        <span>当前免费剩余发文量:  <i className="highlight">{quota.freeNum || 0}篇</i></span>
+        { (!!quota.postRemain) && <span className="left-m">当前剩余信息发布点:  <i className="highlight">{quota.postRemain || 0}点</i></span>}
+        { (!!quota.postRemain) && url() }
+        { (!!quota.postRemain) && <span className="left-m">(消耗顺序： 免费发文&gt;信息发布点，1篇文章消耗6个信息发布点)</span> }
       </div>)
     }
   }
