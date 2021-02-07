d778e76344753672aeb3dca989f819f756de45e2
Kian Zhu <kianzyk@gmail.com>
2021-02-07 15:51:52 +0800

    联系我们

--- assets/site-template-2/pc/home/index.js
+++ assets/site-template-2/pc/home/index.js
@@ -1,8 +1,10 @@
 import './index.styl';
+import $ from 'jquery'
 import Swiper from 'swiper';
 import { qqModule } from '../components/customer-service/index';
 qqModule()
 
+// 轮播图
 new Swiper('#banner-container .swiper-container', {
 	watchSlidesProgress: true,
 	slidesPerView: 'auto',
@@ -23,9 +25,9 @@ new Swiper('#banner-container .swiper-container', {
 				const slideProgress = this.slides[i].progress;
 				let modify = 1;
 				if (Math.abs(slideProgress) > 1) {
-					modify = (Math.abs(slideProgress) - 1) * 0.3 + 1;
+					modify = (Math.abs(slideProgress) - 1) * 0.4 + 1;
 				}
-				const translate = slideProgress * modify * 250 + 'px';
+				const translate = slideProgress * modify * 130 + 'px';
 				const scale = 1 - Math.abs(slideProgress) / 4;
 				const zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
 				slide.transform('translateX(' + translate + ') scale(' + scale + ')');
@@ -45,4 +47,33 @@ new Swiper('#banner-container .swiper-container', {
 	}
 });
 
+function initialize () { 
+  setTimeout(() => {
+		const map = new BMapGL.Map('baidu-map'); 
+		const point = new BMapGL.Point(121.368895,31.13529);  // 创建点坐标 
+		map.centerAndZoom(point, 16);         
+		map.enableScrollWheelZoom(true);
+		// map.addControl(new BMapGL.ZoomControl());
+		// map.addControl(new BMapGL.ScaleControl());
 
+		const marker = new BMapGL.Marker(point);
+		map.addOverlay(marker);
+
+		// 信息窗口
+		const infoWindow = new BMapGL.InfoWindow("地址：上海市闵行区顾戴路2337号维璟中心A栋", {
+			width : 150,
+			title : "百姓网股份有限公司" ,
+		});
+		map.openInfoWindow(infoWindow, point);
+	}, 1000)
+}
+
+// 百度地图
+function loadScript() { 
+  const script = document.createElement("script"); 
+  script.src = "https://api.map.baidu.com/api?v=1.0&type=webgl&ak=tvd0R7EFrjmY7CKT153pdNnTGiEo6Bp4&callback=initialize";
+  document.head.appendChild(script); 
+} 
+
+loadScript();
+initialize();
