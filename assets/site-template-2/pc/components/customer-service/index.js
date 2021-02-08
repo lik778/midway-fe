d7c6f755930631643a48ae8d18a67bac2ccdf778
Kian Zhu <kianzyk@gmail.com>
2021-02-08 10:39:39 +0800

    侧边栏更新

--- assets/site-template-2/pc/components/customer-service/index.js
+++ assets/site-template-2/pc/components/customer-service/index.js
@@ -1,34 +1,45 @@
 import $ from 'jquery'
-import { eventTracker } from '../../../../common/tracker';
+import { eventTracker } from '../../../../common/tracker'
 
 export const qqModule = function() {
 	$(document).ready(function(){
 		$(".qq-title").click(function(){
 			$(".customer-box").hide();
 			$(".customer-hide").show();
-		});
+		})
 		$(".customer-hide").click(function(){
 			$(".customer-box").show();
 			$(".customer-hide").hide();
-		});
+		})
 
 		//点击QQ客服event打点
 		$(".customer-box>dl a").click(()=>{
 			eventTracker("qq-pc", "float-pc")
-		});
+		})
 		//电话打点
 		$(".customer-box .center").click(()=>{
 			eventTracker("phone-pc", "float-pc")
-		});
+		})
 		//微信打点
 		$(".customer-box .footer").click(()=>{
 			eventTracker("wechat-pc", "float-pc")
-		});
+		})
 	})
 }
 
-$('#back-to-top').click(() => {
-	document.body.scrollIntoView({ block: 'start', behavior: 'smooth' })
-})
+const backToTop = $('#back-to-top');
+const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+$(document).scroll(() => {
+	if ($(document).scrollTop() > windowHeight) {
+		backToTop.slideDown();
+	} else {
+		backToTop.slideUp();
+	}
+});
+
+backToTop.click(() => {
+	document.body.scrollIntoView({ block: 'start', behavior: 'smooth' });
+});
 
 
