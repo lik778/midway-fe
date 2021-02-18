690d0e81f358fdefd82f6bc9bbf38bcacaa93e8e
Kian Zhu <kianzyk@gmail.com>
2021-02-18 11:21:08 +0800

    文章详情

--- assets/site-template-2/pc/news-detail/index.js
+++ assets/site-template-2/pc/news-detail/index.js
@@ -1,4 +1,17 @@
 import './index.styl';
 import { qqModule } from '../components/customer-service/index';
+import Swiper from 'swiper';
 
 qqModule()
+
+// 推荐产品轮播
+new Swiper('#product-swiper .swiper-container', {
+	speed: 400,
+	autoplay: {
+		delay: 10000
+	},
+	pagination: {
+    el: '#product-swiper .swiper-pagination',
+    type: 'bullets',
+  },
+});
