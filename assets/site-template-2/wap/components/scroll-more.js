4717b1db724129147194db41e888a2e1622b4513
DJ <dj@Macintosh.local>
2021-03-01 14:46:19 +0800

    ++

--- assets/site-template-2/wap/components/scroll-more.js
+++ assets/site-template-2/wap/components/scroll-more.js
@@ -6,12 +6,19 @@ export const scrollMore = function () {
         const s = $(window).scrollTop()
         const h = $(window).height()
         const d = $(document).height()
-        //console.log("s:",s,"h:",h,"d:",d);
-        if((s+h+100) >= d){
-
+        const page = 1
+        const totalPage = 2
+        const pathname = window.location.pathname
+        const baseUrl = pathname.indexOf("?")== -1 ? pathname : pathname.substring(0, pathname.indexOf("?"))
+        console.log("pathName:",pathname);
+        console.log(baseUrl);
+        console.log("s:",s,"h:",h,"d:",d);
+        if((s+h+100  ) >= d){
+            page++
+            $.ajax({
+            })
         }
-        function loadApi() {
-        }
+        function loadApi() {}
     })
     })
 }
