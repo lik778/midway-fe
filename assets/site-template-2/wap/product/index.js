dd9cbedc18b49f208d6113d4382130628959b0ad
DJ <dj@Macintosh.local>
2021-03-01 14:24:36 +0800

    加scrollmore

--- assets/site-template-2/wap/product/index.js
+++ assets/site-template-2/wap/product/index.js
@@ -1,5 +1,7 @@
 import './index.styl'
 import '../layout/index'
 import { tabModule } from '../components/toptab/index';
+import { scrollMore } from '../components/scroll-more'
 
 tabModule()
+scrollMore()
