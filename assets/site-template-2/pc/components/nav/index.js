fd4921c02360a25a11d87ba6068659dc669a9f55
DJ <dj@Macintosh.local>
2021-03-02 16:24:37 +0800

    [fix]详情页增加高亮

--- assets/site-template-2/pc/components/nav/index.js
+++ assets/site-template-2/pc/components/nav/index.js
@@ -15,18 +15,18 @@ export const navModule = function() {
 		const windowHref = window.location.href;
 		const windowPath = window.location.pathname
 		//定义一个方法：获取当前页面url中的第2个‘/’出现的位置
-		function find(str,cha,num){
-			var x=str.indexOf(cha);
-			for(var i=0;i<num;i++){
-				x=str.indexOf(cha,x+1);
-			}
-			return x;
-			}
-		const targetIndex = find(windowPath,'/',1)
+		//function find(str,cha,num){
+		//	var x=str.indexOf(cha);
+		//	for(var i=0;i<num;i++){
+		//		x=str.indexOf(cha,x+1);
+		//	}
+		//	return x;
+		//	}
+		//const targetIndex = find(windowPath,'/',1)
 		$(".nav-item >a").each((index, tab) => {
 			const h = $(tab).attr('href');
 			const n = h.slice(-2,-1); //提取导航里的尾部，展示n,p
-			const t = windowPath.slice(targetIndex+1, targetIndex+2); //提取当前url的尾部，展示n,p
+			const t = windowPath.slice(1, 2); //提取当前url的尾部，展示n,p
 			console.log("t:",t);
 			console.log("n:",n);
 			console.log("h:",h);
