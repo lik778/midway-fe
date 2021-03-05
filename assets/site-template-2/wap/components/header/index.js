1000cfa6827a0d709b6c4bc72ed2f26001f19daa
DJ <dj@Macintosh.local>
2021-03-05 19:14:33 +0800

    调整wap企业资料布局

--- assets/site-template-2/wap/components/header/index.js
+++ assets/site-template-2/wap/components/header/index.js
@@ -1,29 +1,8 @@
 import $ from 'jquery'
 
 export const menusModule = function() {
-  //const HIDE_STATUS = 'none'
-  //const $navMenusEl = $('.nav-menus')
-
-  //点击导航返回按钮
   $('.back-icon').on('click', function(e) {
     history.back()
   })
-
-  //$('.select-icon').on('click', function(e) {
-  //    const status = $navMenusEl.css('display')
-  //    if (status == HIDE_STATUS) {
-  //      $navMenusEl.show()
-  //    } else {
-  //      $navMenusEl.hide()
-  //    }
-  //    $(document).one("click", function(){
-  //      $navMenusEl.hide()
-  //    })
-  //    e.stopPropagation()
-  //})
-
-  //$navMenusEl.on("click", function(e){
-  //  e.stopPropagation()
-  //})
 }
 
