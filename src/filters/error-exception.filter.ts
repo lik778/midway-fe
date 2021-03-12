ca6ca6fb3f03671f367c13395d7af531b6a66d66
wulei <wulei@baixing.com>
2021-03-12 09:58:27 +0800

    chore: 修改了错误兜底处理

--- src/filters/error-exception.filter.ts
+++ src/filters/error-exception.filter.ts
@@ -1,11 +1,13 @@
 import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
 import { sentryCaptureException } from '../util/sentry';
+import { Response } from 'express';
 
 @Catch(Error)
 export class ErrorExceptionFilter implements ExceptionFilter {
   catch(exception: Error, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
+    const res = ctx.getResponse<Response>();
     sentryCaptureException(exception);
-    ctx.getNext()();
+    res.send('网络异常，请重试')
   }
 }
