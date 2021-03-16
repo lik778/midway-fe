74d3aa68a0d31ea3efce929f0730295813a4405b
wangjiahui <wangjiahui@baixing.com>
2021-03-16 15:05:15 +0800

    基础信息填写

--- app/mvip-manager/src/pages/ai-content/base-information/index.tsx
+++ app/mvip-manager/src/pages/ai-content/base-information/index.tsx
@@ -1,43 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Input , Form , Col , Row  , Button} from 'antd';
 import MainTitle from '@/components/main-title';
 import './index.less';
+import MyModal, { ModalType } from '@/components/modal';
 
 export default (props: any) => {
-
+  const [form] = Form.useForm();
+  const [modalVisible, setModalVisible] = useState<boolean>(false)
   const FormItem = Form.Item;
   const TextArea = Input.TextArea;
 
   const contentName: any = {
     wordA: {
       label: '公司优势',
-      name: 'One',
+      name: 'Advantage',
     },
     wordB: {
       label: '公司好评',
-      name: 'Two',
+      name: 'Praise',
     },
     wordC: {
       label: '公司服务承诺',
-      name: 'Three',
+      name: 'ServicePromise',
     },
     wordD: {
       label: '公司服务产品',
-      name: 'Four',
+      name: 'ServiceProduct',
     },
     wordE: {
       label: '行业小知识',
-      name: 'Five',
+      name: 'Industry knowledge',
       ps:'（回答通用素材库）',
     }
   }
 
+  const printdata = () => {
+    const values = form.getFieldsValue()
+    const groupNames = Object.keys(contentName).map((x: string) => x)
+      Object.keys(values).forEach((k: string) => {
+        if (groupNames.includes(k)) {
+          values[k] = (values[k] && values[k].split('\n')) || []
+        }
+      })
+      console.log(values);
+      setModalVisible(false);
+  }
   return (
     <div>
       <MainTitle title="基础信息素材"/>
       <div className="ai-list-container" style={{padding:'40px 10px'}}>
           <div className="ai-create-information-box">
-            <Form name="base-information-box">
+            <Form name="base-information-box" form={form} onFinish={printdata}>
               <Col className="content-list" span={20}>
                 {
                   Object.keys(contentName).map((k) => {
@@ -61,8 +74,19 @@ export default (props: any) => {
                 }
               </Col>
             </Form>
-            <Button style={{ width: 120, height: 40, background: '#096DD9', borderColor: '#096DD9' }} type="primary" htmlType="submit">提交</Button>
+            <Button style={{ width: 120, height: 40, background: '#096DD9', borderColor: '#096DD9' }} type="primary" onClick={() => setModalVisible(true)} htmlType="submit">提交</Button>
           </div>
+          <MyModal
+            title="确认提交"
+            content="提交后不可修改，确认提交吗？"
+            type={ModalType.info}
+            onCancel={() => setModalVisible(false)}
+            onOk={() => setModalVisible(false)}
+            footer={<div>
+              <Button onClick={() => setModalVisible(false)}>取消</Button>
+              <Button type="primary" onClick={() =>printdata()}>确认</Button>
+            </div>}
+            visible={modalVisible} />
         </div>
     </div>
   )
