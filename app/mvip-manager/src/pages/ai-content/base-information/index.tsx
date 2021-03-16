d474842897f355017092d286cb6f450eebb640a7
wangjiahui <wangjiahui@baixing.com>
2021-03-16 11:34:53 +0800

    基础信息填写

--- app/mvip-manager/src/pages/ai-content/base-information/index.tsx
+++ app/mvip-manager/src/pages/ai-content/base-information/index.tsx
@@ -1,16 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { Input, Tooltip , Form , Col , Row  , Button} from 'antd';
-import { Link } from 'umi';
+import React from 'react';
+import { Input , Form , Col , Row  , Button} from 'antd';
 import MainTitle from '@/components/main-title';
-import Loading from '@/components/loading';
-import AiEditModal from '@/components/ai-edit-modal';
-import { getAiListApi, pauseAiTaskApi, startAiTaskApi } from '@/api/ai-content';
 import './index.less';
-import { AiContentItem } from '@/interfaces/ai-content';
-import { addKeyForListData, formatTime } from '@/utils';
-import { errorMessage } from '@/components/message';
-import { AiTaskAction, AiTaskStatus } from '@/enums';
-import { AiTaskStatusText } from '@/constants';
 
 export default (props: any) => {
 
@@ -37,26 +28,29 @@ export default (props: any) => {
     wordE: {
       label: '行业小知识',
       name: 'Five',
+      ps:'（回答通用素材库）',
     }
   }
 
   return (
     <div>
       <MainTitle title="基础信息素材"/>
-      <div className="ai-list-container">
-          <div className="ai-create-box">
-            <Form name="create-job-form">
-              <Col className="group-words-list" span={24}>
+      <div className="ai-list-container" style={{padding:'40px 10px'}}>
+          <div className="ai-create-information-box">
+            <Form name="base-information-box">
+              <Col className="content-list" span={20}>
                 {
                   Object.keys(contentName).map((k) => {
                     const x = contentName[k];
-                    return (<Row key={k} className="gutter-row" gutter={40} justify="center">
-                        <Col className="content-label" span={2}><div><span style={{color:'red'}}>*</span>{x.label}：</div> </Col>
-                        <Col span={18}>
+                    return (<Row key={k} className="gutter-row" gutter={40} justify="start">
+                        <Col className="content-label" flex="170px" style={{padding:'3px'}}>
+                          <div><span style={{color:'red'}}>*</span>{x.label}：</div> 
+                          <div>{x.ps}</div>
+                        </Col>
+                        <Col className="content-text" flex="800px" style={{padding:0}}>
                         <FormItem name={x.name}>
                           <TextArea rows={5} 
                           placeholder={"一行一个素材，至少填写5个，多个素材以回车分行区别。"} 
-                          maxLength={200} 
                           style={{resize:'none'}}
                           />
                         </FormItem>
@@ -67,7 +61,7 @@ export default (props: any) => {
                 }
               </Col>
             </Form>
-            {/* <Button style={{ width: 120, height: 40, background: '#096DD9', borderColor: '#096DD9' }} type="primary" onClick={() => setModalVisible(true)} htmlType="submit">提交</Button> */}
+            <Button style={{ width: 120, height: 40, background: '#096DD9', borderColor: '#096DD9' }} type="primary" htmlType="submit">提交</Button>
           </div>
         </div>
     </div>
