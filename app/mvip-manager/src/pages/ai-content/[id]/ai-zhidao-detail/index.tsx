029e3b693e74d8c9b646ed1016c7c3c2e7873fad
明胧 <zhaominglong@baixing.com>
2021-03-19 18:12:19 +0800

    [feat]将问答库列表interface修改成后端数据格式

--- app/mvip-manager/src/pages/ai-content/[id]/ai-zhidao-detail/index.tsx
+++ app/mvip-manager/src/pages/ai-content/[id]/ai-zhidao-detail/index.tsx
@@ -2,15 +2,15 @@ import React, { FC, useEffect, useState } from 'react'
 import { TableColumnProps, Table, Spin, Tooltip } from 'antd'
 import { useHistory, useParams } from "umi";
 import MainTitle from '@/components/main-title';
-import { QuestionListItem, AnswerListItem } from '@/interfaces/ai-content'
+import { QuestionListItem, } from '@/interfaces/ai-content'
 import { getQuestionTaskDetailApi, editQuestion } from '@/api/ai-content'
-import { addKeyForListData, formatTime, mockData } from '@/utils';
+import { mockData } from '@/utils';
 import { errorMessage } from '@/components/message';
 import styles from './index.less'
 import EditableRow from './components/editable/row'
 import EditableCell from './components/editable/cell'
 import EditableExpandable from './components/expandable'
-
+import { AiTaskStatus } from '@/enums/index'
 interface JobListDetailProp {
 
 }
@@ -34,11 +34,11 @@ const AiZhidaoDetail: FC<JobListDetailProp> = (props) => {
     },
     {
       title: '问题标题',
-      dataIndex: 'title',
+      dataIndex: 'question',
       onCell: (record: QuestionListItem) => ({
         record,
         editable: pageType === 'edit',
-        dataIndex: 'title',
+        dataIndex: 'question',
         title: '问题标题',
         upDataLoading,
         handleSave: handleSave,
@@ -52,17 +52,21 @@ const AiZhidaoDetail: FC<JobListDetailProp> = (props) => {
     dataIndex: 'status',
     width: 200,
     render: (text: number, record: QuestionListItem, index: number) => {
-      if (text === 0) {
-        return <div className={styles['question-status-0']}>未发布</div>
-      } else if (text === 1) {
-        return <div className={styles['question-status-1']}>已发布</div>
-      } else if (text === 2) {
+      if (text === 2) {
+        return <div className={styles['question-status-1']}>{
+          AiTaskStatus[text]
+        }</div>
+      } else if (text === 3) {
         return (
           <Tooltip title={record.tip}>
             <div className={styles['question-status-2']}>发布失败</div>
             <div className={styles['question-status-2-tip']}>{record.tip}</div>
           </Tooltip>
         )
+      } else {
+        return <div className={styles['question-status-0']}>{
+          AiTaskStatus[text]
+        }</div>
       }
     }
   }]
@@ -76,20 +80,25 @@ const AiZhidaoDetail: FC<JobListDetailProp> = (props) => {
     const data = []
     while (i <= 200) {
       data.push({
-        id: i,
-        title: `这是标题${i}`,
-        answer: [{
-          id: 1,
-          content: '这是答案1'
-        }, {
-          id: 2,
-          content: '这是答案2'
-        }, {
-          id: 3,
-          content: '这是答案3'
-        }],
+        "id": i,
+        "question": "江西优质的冰箱维修怎么收费帮忙回答一下呗",
+        "answers": [
+          "这个价格问题很难说，不能照标准单纯执行有歧义的。反正在我们上海，北京这里，听到百姓网股份他家的名字都是认可的，毕竟也是做了5年年的了。他家的联系方式就是给出的那几个，你往旁边找找看。建议你从预算、质量、环保度、口碑等综合去比较。百姓网股份这家公司真是千载难逢的好企业，对员工的各种培训都很到位，客人结伴去。要找冰箱维修相关商家，要拥有一双火眼金睛，听到看到免费、赠送等字眼别入坑。",
+          "这家百姓网股份，是由{创始人}建立的。他们公司非常正规的，对用户有很多承诺，让我们很放心，给你看看他们的承诺：{服务承诺}。之前我还看过他们的简介：{公司介绍}，也发你了解下，他们提供的服务：我是公司产品我是公司产品，在上海，北京里都是数一数二的，基本上没有什么差评，售后都非常不错，你可以了解下。如果您对这行感兴趣，不妨了解下：",
+          "价格方面无法那么确切，得看你的具体情况而定。之前有个朋友想知道行业信息，我写了一些：鄙人特别支持这位老师写的重要建议，老铁你真牛呀!"
+        ],
+        "wordTypes": "ABCDE",
+        "questionType": "PRICE",
+        "words": [
+          "江西",
+          "优质的",
+          "冰箱维修",
+          "6|||怎么收费|||价格决策",
+          "帮忙回答一下呗"
+        ],
+        "coreWords": "冰箱维修",
         /** 0 待审核，1 已通过，2 未通过 */
-        status: 2 as 2,
+        status: 0 as 0,
         tip: `这是提示这是提示这是提示这是提示这是提示这是提示这是提示这是提示这是提示这是提示这是提示这是提示${i}`
       })
       i++
@@ -146,7 +155,7 @@ const AiZhidaoDetail: FC<JobListDetailProp> = (props) => {
           }} columns={pageType === 'edit' ? columsEdit : columsSee} dataSource={dataList} rowKey="id"
             expandable={{
               expandedRowRender: (record, index) => <EditableExpandable record={record} upDataLoading={upDataLoading} handleSave={handleSave} editable={pageType === 'edit'}></EditableExpandable>,
-              rowExpandable: record => record.answer && record.answer.length > 0,
+              rowExpandable: record => record.answers && record.answers.length > 0,
             }}
             expandedRowClassName={() => styles['expanded-row']}
           ></Table>
