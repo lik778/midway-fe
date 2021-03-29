f9dd231ed65e5cbaa732bb2a5a4325cdb76be73a
wulei <wulei@baixing.com>
2021-03-29 15:31:00 +0800

    chore: 将b2b about逻辑放入b2b controller类中

--- src/filters/index.ts
+++ src/filters/index.ts
@@ -1,12 +1,10 @@
 import { ApiExceptionFilter } from './api-exception.filter';
 import { HttpExceptionFilter } from './http-exception.filter';
 import { SiteExceptionFilter } from './site-exception.filter';
-import { ErrorExceptionFilter } from './error-exception.filter';
 
 export const exceptionFilters = [
     new ApiExceptionFilter(),
     new SiteExceptionFilter(),
     new HttpExceptionFilter(),
-    new ErrorExceptionFilter()
 ]
 
