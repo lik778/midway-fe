be21182f6ba4856be20190b17bd8ffd2c8df795d
DJ <dj@Macintosh.local>
2021-04-23 09:59:59 +0800

    联系方式打点改为hover

--- assets/site-template-2/pc/components/customer-service/index.js
+++ assets/site-template-2/pc/components/customer-service/index.js
@@ -3,25 +3,16 @@ import { eventTracker } from '../../../../common/tracker'
 
 export const qqModule = function() {
 	$(document).ready(function(){
-		$(".qq-title").click(function(){
-			$(".customer-box").hide();
-			$(".customer-hide").show();
-		})
-		$(".customer-hide").click(function(){
-			$(".customer-box").show();
-			$(".customer-hide").hide();
-		})
-
 		//点击QQ客服event打点
-		$(".customer-box>dl a").click(()=>{
+		$(".qq").hover(()=>{
 			eventTracker("qq-pc", "float-pc")
 		})
 		//电话打点
-		$(".customer-box .center").click(()=>{
+		$(".mobile").hover(()=>{
 			eventTracker("phone-pc", "float-pc")
 		})
 		//微信打点
-		$(".customer-box .footer").click(()=>{
+		$(".wechat").hover(()=>{
 			eventTracker("wechat-pc", "float-pc")
 		})
 	})
