429cd03b05ed3a3cac46d45f508a7a3c0d9b3651
明胧 <zhaominglong@baixing.com>
2021-04-30 17:33:52 +0800

    [feat]服务列表

--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -44,7 +44,7 @@ export class SiteService {
       //domain = 'shop-test.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板2，B2B模板，使用这个domain*/
-      domain = 'agui.shop.baixing.cn'
+      domain = 'zmlc2b.shop.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板1，B2C模板，使用这个domain*/
       //domain = 'shop.baixing.cn'
