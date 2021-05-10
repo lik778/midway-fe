357ac30eb48ba58dbe68c884429c38e8302ab207
DJ <dj@Macintosh.local>
2021-05-10 17:27:06 +0800

    [fix]给轮播增加loop,可左点击

--- assets/site-template-3/pc/components/home/product-swiper/index.js
+++ assets/site-template-3/pc/components/home/product-swiper/index.js
@@ -3,10 +3,7 @@ import Swiper from 'swiper';
 export const productSwiper = () => {
   // 首页推荐产品轮播
   new Swiper('#product-swiper-box .swiper-container', {
-    //展示4个
-    //slidesPerView: 4,
     speed: 1500,
-    //spaceBetween: 20,
     //autoplay: {
     //  delay: 10000,
     //  disableOnInteraction: false,
@@ -14,12 +11,11 @@ export const productSwiper = () => {
     //},
     pagination: {
       el: '#product-swiper-box .swiper-pagination',
-      type: 'bullets',
       clickable: true
     },
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
+      nextEl: '#product-swiper-box .swiper-button-next',
+      prevEl: '#product-swiper-box .swiper-button-prev',
     },
   });
 }
