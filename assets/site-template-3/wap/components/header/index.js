16541ca7b67bfa78a8108189009449d97c89b9cd
明胧 <zhaominglong@baixing.com>
2021-05-10 18:50:31 +0800

    [feat]b2cpc wap首页编写

--- assets/site-template-3/wap/components/header/index.js
+++ assets/site-template-3/wap/components/header/index.js
@@ -1,12 +1,8 @@
 import $ from 'jquery'
-import '../drawer-nav/index'
+
 export const menusModule = function() {
   $('.back-icon').on('click', function(e) {
     history.back()
   })
-
-  //$('.header >img').on('click', function () {
-
-  //})
 }
 
