555e83574ab7a61eb55991c75b24d8dde9569a92
Irisss <oncwnuDk6gOwuB941oBlixUVzBN4@git.weixin.qq.com>
2021-05-11 15:20:53 +0800

    [feat]B2C模板WAP端底部联系方式

--- assets/site-template-3/wap/components/footer-contact/index.js
+++ assets/site-template-3/wap/components/footer-contact/index.js
@@ -1,17 +1,33 @@
 import $ from 'jquery'
 import { eventTracker } from '../../../../common/tracker';
 
+function copyToClip(content, message) {
+  var aux = document.createElement("input");
+  aux.setAttribute("value", content);
+  document.body.appendChild(aux);
+  aux.select();
+  document.execCommand("copy");
+  document.body.removeChild(aux);
+  alert(message || "复制成功");
+}
+
 export const footerContactModule = function(){
 	//等待DOM 结构加载完成，然后再执行操作
 	$(document).ready(function(){
-		$(".wechat-box").click(function(){
-			$(".dialog").css("display","block");
-			$(".mask").show()
-		});
-		$(".dialog > .cancel").click(function(){
-			$(".dialog").css("display","none");
-			$(".mask").hide()
-		});
+		// $(".wechat-box").click(function(){
+		// 	$(".dialog").css("display","block");
+		// 	$(".mask").show()
+		// });
+		// $(".dialog > .cancel").click(function(){
+		// 	$(".dialog").css("display","none");
+		// 	$(".mask").hide()
+		// });
+
+		$(".wechat-box").on('click',function(){
+			const data = $(this).data('value')
+			if ( data != '' )
+				copyToClip(data)
+		})
 
 		//点击底部浮层打点
 		$(".sms-box").click(()=>{
