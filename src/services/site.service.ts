e527160ee25f8a586741cfb9f80228174b8922a3
DJ <dj@Macintosh.local>
2021-05-13 14:41:41 +0800

    fix 轮播图分页颜色

--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -42,7 +42,7 @@ export class SiteService {
       //domain = 'agui.shop-test.baixing.cn'
 
       /*后端在test分支，且店铺类型是是模板1，B2C模板，使用这个domain*/
-      domain = 'shop-test.baixing.cn'
+      domain = 'agui.shop-test.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板2，B2B模板，使用这个domain*/
       // domain = 'zmlc2b.shop.baixing.cn'
