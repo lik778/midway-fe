dd418fab397b3f56103c289e469e6dc251798c99
明胧 <zhaominglong@baixing.com>
2021-05-14 11:54:26 +0800

    [feat]新优化

--- assets/site-template-3/wap/components/drawer-nav/index.js
+++ assets/site-template-3/wap/components/drawer-nav/index.js
@@ -3,30 +3,31 @@ import $ from 'jquery'
 export const initNav = function () {
   const navSelectIcon = $('.nav-select-icon')
   const drawerNavBox = $('.drawer-nav-box')
+  const drawerNavMask = $('.drawer-nav-mask')
   const body = $('body')
   const html = $('html')
   body.css({ transition: "all 0.5s" });
 
   navSelectIcon.on('click', function () {
-    var selectIconClassName = navSelectIcon.attr('class')
-    if (selectIconClassName.indexOf('active') !== -1) {
-      navSelectIcon.removeClass('active')
-      drawerNavBox.removeClass('active')
-      body.css({ transform: '' });
-      html.css({ width: 'auto', height: 'auto', overflow: 'visible' })
-    } else {
-      navSelectIcon.addClass('active')
-      drawerNavBox.addClass('active')
-      body.css({ transform: 'translate(180px)' });
-      html.css({ width: '100vw', height: '100vh', overflow: 'hidden' })
-    }
+    navSelectIcon.addClass('active')
+    drawerNavBox.addClass('active')
+    drawerNavMask.addClass('active')
+    body.css({ transform: 'translate(180px)' });
+    html.css({ width: '100vw', height: '100vh', overflow: 'hidden' })
+  })
+  drawerNavMask.on('click', function () {
+    navSelectIcon.removeClass('active')
+    drawerNavBox.removeClass('active')
+    drawerNavMask.removeClass('active')
+    body.css({ transform: '' });
+    html.css({ width: 'auto', height: 'auto', overflow: 'visible' })
   })
 
   const imgSrc = {
-    0:'//file.baixing.net/202105/ebad606fef0001ec73d15fc4e51921c4.png',
-    1:'//file.baixing.net/202105/609ea6ca5dc441be4501410db0509cfe.png',
-    2:'//file.baixing.net/202105/1cdccfd92f8c5e86ae808e362cf2cd80.png',
-    3:'//file.baixing.net/202105/dac0b607fef01e3a1ebd03a440a1922a.png',
+    0: '//file.baixing.net/202105/ebad606fef0001ec73d15fc4e51921c4.png',
+    1: '//file.baixing.net/202105/609ea6ca5dc441be4501410db0509cfe.png',
+    2: '//file.baixing.net/202105/1cdccfd92f8c5e86ae808e362cf2cd80.png',
+    3: '//file.baixing.net/202105/dac0b607fef01e3a1ebd03a440a1922a.png',
   }
   $(document).on('ready', function () {
     const windowHref = window.location.href;
@@ -37,8 +38,8 @@ export const initNav = function () {
       const t = windowPath.slice(1, 2); //提取当前url的尾部，展示n,p
       if (h == windowHref || t == n || windowPath.indexOf(h) !== -1) {
         $(tab).addClass('active')
-        const imgList =$(tab).find('img')
-        if(imgList&&imgList.length>0){
+        const imgList = $(tab).find('img')
+        if (imgList && imgList.length > 0) {
           $(imgList[0]).attr('src', imgSrc[index])
         }
       }
