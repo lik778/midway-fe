5b28b2b12a48b216cd90e67966ec2d8d5c250630
wulei <wulei@baixing.com>
2021-05-18 10:14:17 +0800

    chore: 对店铺后台嵌入页面iframe进行处理

--- app/mvip-manager/src/wrappers/path-auth.tsx
+++ app/mvip-manager/src/wrappers/path-auth.tsx
@@ -4,18 +4,19 @@ import { connect } from 'dva'
 import { isNull } from 'lodash'
 import { baseMapStateToProps } from '@/models/base'
 import { Redirect } from 'umi'
+import { notInIframe, inIframe } from '@/utils'
 import MainTitle from '@/components/main-title'
 
 
 const PathAuth = (props: any) => {
   const { route: { path: urlPath }, menuAuthList } = props
 
-  if (isNull(menuAuthList)) return <>
+  if (notInIframe() && isNull(menuAuthList)) return <>
     <MainTitle title="加载中..."/>
     <div className="container"><Skeleton active paragraph={{ rows: 12 }} /></div>
   </>
 
-  if (menuAuthList.some((x: string) => urlPath.includes(x))) {
+  if (inIframe() || menuAuthList.some((x: string) => urlPath.includes(x))) {
     return <div>{ props.children }</div>
   }
 
