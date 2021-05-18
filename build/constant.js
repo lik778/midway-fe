d38ca3ab8611a29fe96a7803f45cd4b507a01edf
明胧 <zhaominglong@baixing.com>
2021-05-18 15:48:22 +0800

    about wap事件初始化

--- build/constant.js
+++ build/constant.js
@@ -12,7 +12,7 @@ const TB_TYPE_B2C_3 = 3
 const TB_BASE_PAGE_NAMES = ['home', 'news', 'news-detail', 'product', 'product-detail']
 const TB_PAGE_NAMES_B2C_1 = [...TB_BASE_PAGE_NAMES, 'news-child', 'product-child']
 const TB_PAGE_NAMES_B2B_2 = [...TB_BASE_PAGE_NAMES, 'about']
-const TB_PAGE_NAMES_B2C_3 = [...TB_BASE_PAGE_NAMES,'about']
+const TB_PAGE_NAMES_B2C_3 = [...TB_BASE_PAGE_NAMES, 'about']
 
 module.exports = {
   DEVICE_TYPE,
