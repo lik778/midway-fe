b5e04582006881839884379801befaafc0669d22
wangjiahui <wangjiahui@baixing.com>
2021-05-18 15:51:38 +0800

    [feat]B2C模板PC端产品服务详情页查看电话

--- assets/site-template-3/pc/product-detail/index.js
+++ assets/site-template-3/pc/product-detail/index.js
@@ -4,9 +4,10 @@ import '../layout/index';
 //import { navModule } from '../components/nav'
 import { viewTel } from '../components/product-detail/company-top'
 import { leaveLeads } from '../components/contact-us'
-
+import { viewPhone } from '../components/contact-form-companyInfo';
 leaveLeads()
 //navModule()
 viewTel()
 //qqModule()
+viewPhone()
 
