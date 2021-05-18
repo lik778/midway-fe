2d9fc549482b01cec9ebbf7b24342dd27204a534
DJ <dj@Macintosh.local>
2021-05-18 17:02:35 +0800

    [feat]调整b2b模板首页头和公司信息

--- assets/site-template-2/wap/home/index.js
+++ assets/site-template-2/wap/home/index.js
@@ -3,7 +3,10 @@ import '../layout/index'
 import '../components/swiper/index'
 import { aboutModule } from '../components/aboutus/index'
 import { leaveLeads } from '../components/contact-form/index'
+import { initCompanyInfo } from '../components/company-info-home/index'
+
 
 aboutModule()
 leaveLeads()
+initCompanyInfo()
 
