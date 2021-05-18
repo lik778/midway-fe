5e5c1e68eb01d405426824de0d306a197a9c5380
明胧 <zhaominglong@baixing.com>
2021-05-18 17:53:03 +0800

    检查模板

--- assets/site-template-3/pc/about/index.js
+++ assets/site-template-3/pc/about/index.js
@@ -1,4 +1,5 @@
 import './index.styl'
+import '../layout/index';
 import { navModule } from '../components/nav'
 import { leaveLeads } from '../components/contact-us'
 import { viewPhone } from '../components/contact-form-companyInfo';
