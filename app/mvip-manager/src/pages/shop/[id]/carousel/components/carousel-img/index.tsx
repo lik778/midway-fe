3b75b7f15e6ef3d1d52be021c5c88d0d721e86ed
DJ <dj@Macintosh.local>
2021-05-24 22:32:45 +0800

    [feat]后台上传不同尺寸banner

--- app/mvip-manager/src/pages/shop/[id]/carousel/components/carousel-img/index.tsx
+++ app/mvip-manager/src/pages/shop/[id]/carousel/components/carousel-img/index.tsx
@@ -1,33 +1,59 @@
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.less';
 import { BannerImgUpload } from '../banner-img-upload';
-import { createBannerApi, deleteBannerApi } from '@/api/shop';
+import { createBannerApi, getBannerListApi, deleteBannerApi } from '@/api/shop';
 import { useParams } from 'umi';
 import { RouteParams } from '@/interfaces/shop';
-import { DeviceType } from '@/enums';
+import { DeviceType, PositionType} from '@/enums';
 import { errorMessage, successMessage } from '@/components/message';
+import Loading from '@/components/loading';
+import EmptyStatus from '@/pages/shop/components/empty-status';
+
 interface Props {
-  onChange(type: DeviceType): void;
-  fileList:any[];
   type:DeviceType,
+  position: number,
   txt: string,
   tip: string,
 }
+
 export default (props: Props) => {
-  const { onChange, fileList, type, txt, tip } = props
+  const [bannerList, setBannerList] = useState<any[]>([])
+  const [isLoading, setIsLoading] = useState(true)
+  const {type, txt, tip, position } = props
   // 获取店铺id
   const params: RouteParams = useParams();
+
   const createBannerImg = async (url: string) => {
     const res = await createBannerApi(Number(params.id), {
       url,
       type,
+      position
     })
     if(res?.success){
       successMessage('上传成功');
-      onChange(type)
     }else{
       errorMessage(`上传失败:${res?.message}`);
+    }
+  }
+
+  const getBannerList = async () => {
+    const res = await getBannerListApi(Number(params.id), {
+      page: 1,
+      size: 5,
+      status: 1,
+      type,
+      position
+    })
+    if (res?.success) {
+      const list = res.data.result
+      list.map((l: any) => {
+        l['uid'] = l.id.toString()
+        l['url'] = l.displayImgUrl
+        l['status'] = 'done'
+      })
+        setBannerList(list)
+        setIsLoading(false)
     }
   }
 
@@ -37,7 +63,7 @@ export default (props: Props) => {
     })
     if(res?.success){
       successMessage('删除成功');
-      onChange(type)
+      getBannerList()
     }else{
       errorMessage(res?.message);
     }
@@ -51,15 +77,32 @@ export default (props: Props) => {
       deleteBannerImg(id)
     }
   }
-  return (
-    <div className="all-img">
-      <span className="title">{txt}: </span>
-      <div className="img-list">
-        <p className="red-tip tip">严禁上传侵权图片，被控侵权百姓网不承担任何责任，需用户自行承担</p>
-        <p className="tip">{tip}</p>
-        <BannerImgUpload imgType={'text'} text={'上传图片'} maxLength={5} disableBtn={true} onChange={onImgChange} fileList={fileList}/>
-      </div>
-    </div>
+  useEffect(() => {
+    getBannerList()
+  }, [])
 
-  );
-}
+  //const emptyImgs = () => {
+  //  if (bannerList.length === 0) {
+  //    const emptyMsg = {
+  //      img: '//file.baixing.net/202012/c201a04d7d3ac516b3598eb3eb6bd4c1.png',
+  //      msg: '暂无图片，建议上传自己的品牌图片'
+  //    }
+  //    return <EmptyStatus emptyMsg={emptyMsg} />
+  //  }
+  //}
+
+  if (isLoading) {
+    return <Loading />
+  } else {
+    return (
+      <div className="all-img">
+        <span className="title">{txt}: </span>
+        <div className="img-list">
+          <p className="red-tip tip">严禁上传侵权图片，被控侵权百姓网不承担任何责任，需用户自行承担</p>
+          <p className="tip">{tip}</p>
+          <BannerImgUpload imgType={'text'} text={'上传图片'} maxLength={5} disableBtn={true} onChange={onImgChange} fileList={bannerList}/>
+          {/*{emptyImgs()}*/}
+        </div>
+      </div>
+    );
+    }}
--- app/mvip-manager/src/pages/shop/[id]/carousel/index.tsx
+++ app/mvip-manager/src/pages/shop/[id]/carousel/index.tsx
@@ -1,85 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import CarouselImg from './components/carousel-img';
 import BasisHeader from '../components/basis-header';
 import { ShopBasisType, DeviceType } from '@/enums';
 import './index.less'
-import { getBannerListApi } from '@/api/shop';
-import { useParams } from 'umi';
-import { RouteParams } from '@/interfaces/shop';
-import Loading from '@/components/loading';
-import EmptyStatus from '@/pages/shop/components/empty-status';
-import WrapperShopPage from '@/pages/shop';
 
 const ShopCarouselPage = (props: any) => {
-  const [wapBannerList, setWapBannerList] = useState<any[]>([])
-  const [pcBannerList, setPCBannerList] = useState<any[]>([])
-  // 获取店铺id
-  const params: RouteParams = useParams();
-  // isLoaded
-  const [isWapLoading, setIsWapLoading] = useState(true)
-  const [isPcLoading, setIsPcLoading] = useState(true)
-  const getBannerList = async (type: DeviceType) => {
-    const res = await getBannerListApi(Number(params.id), {
-      page: 1,
-      size: 5,
-      status: 1,
-      type,
-    })
-    if (res?.success) {
-      const list = res.data.result
-      list.map((l: any) => {
-        l['uid'] = l.id.toString()
-        l['url'] = l.displayImgUrl
-        l['status'] = 'done'
-      })
-      if (type === DeviceType.PC) {
-        setPCBannerList(list)
-        setIsPcLoading(false)
-      } else if (type === DeviceType.WAP) {
-        setWapBannerList(list)
-        setIsWapLoading(false)
-      }
-
-    }
-  }
-
-  const onChange = (type: DeviceType) => {
-    getBannerList(type)
-  }
-
-  const emptyImgs = () => {
-    if (wapBannerList.length === 0 && pcBannerList.length === 0) {
-      const emptyMsg = {
-        img: '//file.baixing.net/202012/c201a04d7d3ac516b3598eb3eb6bd4c1.png',
-        msg: '暂无图片，建议上传自己的品牌图片'
-      }
-      return <EmptyStatus emptyMsg={emptyMsg} />
-    }
-  }
-
   const imgContainer = () => {
-    if (isWapLoading || isPcLoading) {
-      return <Loading />
-    } else {
-      return (
-        <div className="c-main">
-          <div className="c-pc">
-            <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸1920*360'} txt={'PC端轮播图'} type={DeviceType.PC} fileList={pcBannerList} onChange={onChange} />
-          </div>
-          <div className="c-wap">
-            <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸750*304'} txt={'WAP端轮播图'} type={DeviceType.WAP} fileList={wapBannerList} onChange={onChange} />
-          </div>
-          {emptyImgs()}
+    return (
+      <div className="c-main">
+        <div className="c-pc">
+          <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸1920*540'} txt={'PC端首页轮播图'} type={DeviceType.PC} position={1}/>
+          <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸1920*360'} txt={'PC端内容轮播图'} type={DeviceType.PC} position={3}/>
         </div>
-      )
-    }
+        <div className="c-wap">
+          <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸750*750'} txt={'WAP端首页轮播图'} type={DeviceType.WAP} position={1}/>
+          <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸750*304'} txt={'WAP端内容轮播图'} type={DeviceType.WAP} position={3}/>
+        </div>
+      </div>
+    )
   }
-
-  useEffect(() => {
-    getBannerList(DeviceType.PC)
-    getBannerList(DeviceType.WAP)
-  }, [])
-
   return (
     <>
       <BasisHeader {...props} type={ShopBasisType.CAROUSEL} />
@@ -89,7 +28,6 @@ const ShopCarouselPage = (props: any) => {
     </>
   );
 }
-
 
 ShopCarouselPage.wrappers = ['@/wrappers/path-auth']
 
