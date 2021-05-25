c13bcfef5886bbf9fe8b1ae06da0fbe6c27cbcdd
明胧 <zhaominglong@baixing.com>
2021-05-25 18:20:58 +0800

    修改bug

--- assets/sem/pc/components/home/product-swiper/index.js
+++ assets/sem/pc/components/home/product-swiper/index.js
@@ -1,19 +1,26 @@
+import $ from 'jquery';
 import Swiper from 'swiper';
 
 export const productSwiper = () => {
-  // 首页推荐产品轮播
-  new Swiper('.product-swiper-box .swiper-container', {
-    speed: 1500,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-      waitForTransition: true
-    },
-    pagination: {
-      el: '.product-swiper-box .swiper-pagination',
-      clickable: true,
-      bulletActiveClass:'product-swiper-pagination-active',
-      bulletClass:'product-swiper-pagination'
-    },
-  });
+  $(document).on('ready', function () {
+    new Swiper('.product-swiper-box .swiper-container', {
+      speed: 1500,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        waitForTransition: true
+      },
+      pagination: {
+        el: '.product-swiper-box .swiper-pagination',
+        clickable: true,
+        bulletActiveClass: 'product-swiper-pagination-active',
+        bulletClass: 'product-swiper-pagination'
+      },
+    });
+
+    $('.vad-btn').on('click', function () {
+      const phone = $(this).data('phone')
+      alert(`请复制手机号：${phone}`)
+    })
+  })
 }
--- src/config/local.ts
+++ src/config/local.ts
@@ -2,8 +2,8 @@ export default {
   env: 'local',
   services: {
     'midway-service': {
-      // host: 'http://172.30.2.14:30257'
-      host: 'http://172.17.1.51:8084'
+      host: 'http://172.30.2.14:31257'
+      // host: 'http://172.17.1.51:8084'
     },
     'zhidao-service': {
       host: 'http://172.30.2.14:31252'
