ef14c5ee3134852b3c97043ee5a62c17094b788c
DJ <dj@Macintosh.local>
2021-05-27 17:08:24 +0800

    [fix]输入框

---
+++ assets/common/wap/offical-topbar/index.js
@@ -0,0 +1,16 @@
+
+import $ from 'jquery';
+
+export const initWhiteBlock = () => {
+  $(document).on('ready', function () {
+    $(".search-query").on("keypress", function(e) {
+      var value = $.trim($(this).val());
+
+    //当e.keyCode的值为13 即，点击前往/搜索 按键时执行以下操作
+      if(e.keyCode == 13) {
+        document.activeElement.blur();//软键盘收起
+        window.location.href = `https://china.baixing.com/m/root/?query=${(value || '').toString()}`
+      }
+    });
+  })
+}
