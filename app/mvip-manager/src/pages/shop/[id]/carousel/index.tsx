6f7beaa5b01be1006ce7e41c70cc76c2e85473f5
DJ <dj@Macintosh.local>
2021-05-28 14:14:08 +0800

    [fix]banner不同店铺展示不同组件

--- app/mvip-manager/src/pages/shop/[id]/carousel/index.tsx
+++ app/mvip-manager/src/pages/shop/[id]/carousel/index.tsx
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import CarouselImg from './components/carousel-img';
 import BasisHeader from '../components/basis-header';
-import { ShopBasisType, DeviceType } from '@/enums';
-import './index.less'
+import { ShopBasisType, DeviceType, ProductType  } from '@/enums';
+import './index.less';
+import { connect } from 'dva';
+import { SHOP_NAMESPACE } from '@/models/shop';
+import { RouteParams, ShopInfo } from '@/interfaces/shop';
+
 
 const ShopCarouselPage = (props: any) => {
-  const imgContainer = () => {
+  const [imgContainer, setImgContainer] = useState<React.ReactNode>()
+  const b2cImgContainer = () => {
     return (
       <div className="c-main">
         <div className="c-pc">
@@ -19,11 +24,39 @@ const ShopCarouselPage = (props: any) => {
       </div>
     )
   }
+
+  const b2bImgContainer = () => {
+    return (
+      <div className="c-main">
+        <div className="c-pc">
+          <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸1920*540'} txt={'PC端轮播图'} type={DeviceType.PC} position={1}/>
+        </div>
+        <div className="c-wap">
+          <CarouselImg tip={'最多上传5张轮播图，图片格式：jpg/jpeg/png,大小不超过3M, 建议上传尺寸750*750'} txt={'WAP端轮播图'} type={DeviceType.WAP} position={1}/>
+        </div>
+      </div>
+    )
+  }
+
+  //当前店铺是B2B,B2C，展示不同的上传图片组件
+  const { curShopInfo } = props
+  useEffect(()=>{
+    if (curShopInfo) {
+      const { type } = curShopInfo
+      if (type === ProductType.B2B){
+        setImgContainer(b2bImgContainer)
+      }else{
+        setImgContainer(b2cImgContainer)
+      }
+    }
+  },[curShopInfo])
+
+
   return (
     <>
       <BasisHeader {...props} type={ShopBasisType.CAROUSEL} />
       <div className="container">
-        {imgContainer()}
+        {imgContainer}
       </div>
     </>
   );
@@ -31,4 +64,10 @@ const ShopCarouselPage = (props: any) => {
 
 ShopCarouselPage.wrappers = ['@/wrappers/path-auth']
 
-export default ShopCarouselPage
+//export default ShopCarouselPage
+
+//取状态管理里的当前店铺信息，用于判断店铺类型
+export default connect<{ curShopInfo: ShopInfo }>((state: any): any => {
+  const { curShopInfo } = state[SHOP_NAMESPACE]
+  return { curShopInfo }
+})(ShopCarouselPage)
