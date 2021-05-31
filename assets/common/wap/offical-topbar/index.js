ee897ff93aef231234ac9e156bb77573c55dff12
DJ <dj@Macintosh.local>
2021-05-31 16:25:13 +0800

    [fix]按照度娘要求,面包屑移到底部,搜索再加上,但不能搜

--- assets/common/wap/offical-topbar/index.js
+++ assets/common/wap/offical-topbar/index.js
@@ -6,11 +6,13 @@ export const initWhiteBlock = () => {
     $(".search-query").on("keypress", function(e) {
       var value = $.trim($(this).val());
 
+    //由于度娘要求店铺搜索结果是产品或新闻，所以先下掉跳到主站搜索页
+
     //当e.keyCode的值为13 即，点击前往/搜索 按键时执行以下操作
-      if(e.keyCode == 13) {
-        document.activeElement.blur();//软键盘收起
-        window.location.href = `https://china.baixing.com/m/root/?query=${(value || '').toString()}`
-      }
+      //if(e.keyCode == 13) {
+      //  document.activeElement.blur();//软键盘收起
+      //  window.location.href = `https://china.baixing.com/m/root/?query=${(value || '').toString()}`
+      //}
     });
   })
 }
