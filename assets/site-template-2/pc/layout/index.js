3edf9a54700479b4b36674d271fe44bbad3142e5
明胧 <zhaominglong@baixing.com>
2021-06-01 13:13:02 +0800

    店铺pc顶部修改

--- assets/site-template-2/pc/layout/index.js
+++ assets/site-template-2/pc/layout/index.js
@@ -1,6 +1,7 @@
 import '../../../common/pc/official-old-header'
+// import { initTopbar } from '../../../common/pc/official-topbar'
 import { navModule } from '../components/header'
 import { qqModule } from '../../../common/pc/customer-service';
-
+// initTopbar()
 qqModule()
 navModule()
