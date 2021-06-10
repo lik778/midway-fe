fefde482494bd666baf0d5eceed7ef168714c952
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-06-10 18:15:06 +0800

    feat:店铺接sdk

--- src/controllers/analytics.controller.ts
+++ src/controllers/analytics.controller.ts
@@ -2,17 +2,17 @@ import { Controller, Post, Res, Body } from "@nestjs/common";
 import { Response } from 'express';
 import { SiteService } from '../services/site.service';
 
-@Controller({ path: '/' })
+@Controller({ path: '/api/shield' })
 export class AnalyticsController {
     constructor(protected readonly midwayApiService: SiteService) {}
     //  滑块验证码接口node层转发
-   @Post('/api/shield/web/check')
+   @Post('/web/check')
    async check (@Body() body, @Res() res: Response) {
     const resData = await this.midwayApiService.shieldCheck(body)
     res.json(resData)
    }
  
-   @Post('/api/shield/web/get')
+   @Post('/web/get')
    async get (@Body() body,  @Res() res: Response) {
      console.log('body', body)
      const resData = await this.midwayApiService.shieldGet(body)
