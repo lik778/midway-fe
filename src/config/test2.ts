84c0c0587414d93e1e15fa362ceaf0fcde17623d
baixingwang <1234>
2021-06-18 18:39:19 +0800

    新增一套测试环境test2

---
+++ src/config/test2.ts
@@ -0,0 +1,21 @@
+export default {
+  env: 'test1',
+  services: {
+    'midway-service': {
+      host: 'http://172.30.2.14:30263'
+    },
+    'zhidao-service': {
+      host: 'http://172.30.2.14:30252'
+    },
+    'reporting-service': {
+      host: 'http://dev-api.baixing.cn'
+    }
+  },
+  cdnPath: '',
+  haojing: 'http://wulei.baixing.cn',
+  haojingDomain: 'baixing.cn',
+  hostType: {
+    prefix: ':shopName.shop-test1.baixing.cn',
+    base: 'shop-test1.baixing.cn'
+  }
+}
