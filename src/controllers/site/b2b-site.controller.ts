5aa7f9231746ef1b8d77abee35bba1d76763424c
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-06-18 16:49:45 +0800

    feat:b2b模板增加ip拦截

--- src/controllers/site/b2b-site.controller.ts
+++ src/controllers/site/b2b-site.controller.ts
@@ -1,12 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { BaseSiteController } from './base-site.controller';
 import { SiteService } from '../../services/site.service';
 import config from '../../config'
 import { DomainTypeEnum } from '../../enums';
 import { TrackerService } from '../../services/tracker.service';
+import { UserGuard } from 'src/util/httpInterceptors';
 
 //继承自base-site.controller,用于b2b路由
 @Controller({ host: config().hostType.prefix })
+@UseGuards(UserGuard)
 export class B2bSiteController extends BaseSiteController {
   constructor(protected readonly midwayApiService: SiteService,
              protected readonly trackerService: TrackerService) {
