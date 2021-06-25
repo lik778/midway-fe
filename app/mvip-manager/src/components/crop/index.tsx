e1816e697271cace720f916dfc660889227362e2
明胧 <zhaominglong@baixing.com>
2021-06-25 10:14:41 +0800

    [feat]图片裁剪，延时裁剪插件的出现时机，保证显示

--- app/mvip-manager/src/components/crop/index.tsx
+++ app/mvip-manager/src/components/crop/index.tsx
@@ -6,7 +6,7 @@ import { Button } from 'antd';
 import { errorMessage } from '../message';
 import { upImageToYoupai } from '@/api/common'
 import { CropProps } from './data'
-
+import { Spin } from 'antd'
 interface Props {
   cropProps: CropProps,
   url: string,
@@ -15,6 +15,8 @@ interface Props {
 
 const Crop: FC<Props> = (props) => {
   const { cropProps, url, handleCropSuccess } = props
+  /** 因为mac弹窗出来的时候可能dom节点还没渲染好，所以裁剪块会缩小在左上角，所以延时等弹窗渲染完毕 */
+  const [initFlag, setInitFlag] = useState<boolean>(false)
   const [updataLoading, setUpdataLoading] = useState<boolean>(false)
   const [cropper, setCropper] = useState<Cropper>();
   const [timestamp] = useState<number>(() => new Date().getTime())
@@ -49,39 +51,48 @@ const Crop: FC<Props> = (props) => {
     }/*, 'image/png' */);
   };
 
-  return <div className={styles["crop-container"]}>
-    <div className={styles["crop-content"]}>
-      <Cropper
-        {...cropProps}
-        src={url}
-        style={{ width: 800, height: 600 }}
-        preview={`.img-preview-${timestamp}`}
-        guides={true}
-        autoCropArea={1}
-        movable={true} // 是否允许移动图片
-        zoomTo={1}
-        viewMode={2}
-        minCropBoxHeight={50}
-        minCropBoxWidth={50}
-        background={false}
-        responsive={true}
-        cropBoxResizable={true}
-        cropBoxMovable={true}
-        dragMode='move'
-        checkOrientation={true}
-        onInitialized={(instance) => {
-          setCropper(instance);
-        }}
-      />
-    </div>
-    <div className={styles["crop-preview"]}>
-      <div className={styles["title"]}>图片预览</div>
-      <div className={styles["preview-box"]}>
-        <div className={`${styles["img-preview"]} img-preview-${timestamp}`} />
-      </div>
-      <Button size="large" type="primary" onClick={handleConfirmCropper} disabled={updataLoading} loading={updataLoading}>确认裁剪</Button>
-    </div>
+  useEffect(() => {
+    setTimeout(() => {
+      setInitFlag(true)
+    }, 0)
+  }, [])
 
-  </div>
+  return <Spin spinning={!initFlag}>
+    <div className={styles["crop-container"]}>
+      <div className={styles["crop-content"]}>
+        {
+          initFlag && <Cropper
+            {...cropProps}
+            src={url}
+            style={{ width: 800, height: 600 }}
+            preview={`.img-preview-${timestamp}`}
+            guides={true}
+            autoCropArea={1}
+            movable={true} // 是否允许移动图片
+            zoomTo={1}
+            viewMode={2}
+            minCropBoxHeight={50}
+            minCropBoxWidth={50}
+            background={false}
+            responsive={true}
+            cropBoxResizable={true}
+            cropBoxMovable={true}
+            dragMode='move'
+            checkOrientation={true}
+            onInitialized={(instance) => {
+              setCropper(instance);
+            }}
+          />
+        }
+      </div>
+      <div className={styles["crop-preview"]}>
+        <div className={styles["title"]}>图片预览</div>
+        <div className={styles["preview-box"]}>
+          <div className={`${styles["img-preview"]} img-preview-${timestamp}`} />
+        </div>
+        <Button size="large" type="primary" onClick={handleConfirmCropper} disabled={updataLoading} loading={updataLoading}>确认裁剪</Button>
+      </div>
+    </div>
+  </Spin>
 }
 export default Crop
