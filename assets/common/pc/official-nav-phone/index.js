a5b3820a9800db64e34f589e6b11a3837989b7df
明胧 <zhaominglong@baixing.com>
2021-06-29 10:02:17 +0800

    组件逻辑优化，增加样式隔离

--- assets/common/pc/official-nav-phone/index.js
+++ assets/common/pc/official-nav-phone/index.js
@@ -4,7 +4,7 @@ import { get400Number } from '../../get400Number'
 // 定时刷新倒计时
 const set400NumbertimeOut = function () {
   let number = 180
-  const phoneNumTipText = $('.header-bgc .phone-num-box>.phone-num-tip>.tip-text')
+  const phoneNumTipText = $('.official-nav-phone-num-box>.official-nav-phone-num-tip>.tip-text')
   const timer = setInterval(function () {
     --number;
     if (number > 0) {
@@ -21,11 +21,11 @@ export const initPhone = function () {
   $(document).on('ready', async function () {
     // 如果是sem页面 有点击显示电话功能
     if (isSem) {
-      const phoneNumBox = $('.header-bgc .phone-num-box')
-      const phoneNum = $('.header-bgc .phone-num-box>.phone-num')
-      const phoneNumTip = $('.header-bgc .phone-num-box>.phone-num-tip')
-      const phone400Action = $('.header-bgc .phone-400-action')
-      const phone400ActionBtn = $('.header-bgc .phone-400-action .btn')
+      const phoneNumBox = $('.official-nav-phone-num-box')
+      const phoneNum = $('.official-nav-phone-num-box>.official-nav-phone-num')
+      const phoneNumTip = $('.official-nav-phone-num-box>.official-nav-phone-num-tip')
+      const phone400Action = $('.official-nav-phone-400-action')
+      const phone400ActionBtn = $('.official-nav-phone-400-action .btn')
       phone400ActionBtn.on('click', async function (e) {
         var union400num = $(this).data('union400num')
         if (union400num) {
