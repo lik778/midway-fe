038a357f34b9d48b63de57a1b2cb7254c5049d32
仿生狮子（一光） <tangnad@qq.com>
2021-07-05 20:50:30 +0800

    fix: hooks 无限循环

--- app/mvip-manager/src/pages/shop/[id]/imageset/hooks/albums.ts
+++ app/mvip-manager/src/pages/shop/[id]/imageset/hooks/albums.ts
@@ -4,18 +4,23 @@ import { AlbumItem } from '../types'
 
 export function useAllAlbumLists() {
   const [lists, setLists] = useState<AlbumItem[]>([])
-  new Promise(resolve => setTimeout(resolve, 500)).then(() => {
-    setLists(Array(80).fill('').map((x, idx) => {
-      return {
-        type: 'album',
-        id: idx + 1,
-        name: '默认相册' + idx,
-        count: ~~(Math.random() * 29),
-        url: (idx % 1)
-          ? `http://img4.baixing.net/cda4411639701a0745b0513f968736f8.png_sv1?x=${idx + 1}`
-          : "http://img4.baixing.net/63becd57373449038fcbc3b599aecc8c.jpg_sv1",
-      }
-    }))
-  })
+
+  useEffect(() => {
+    console.log('useAllAlbumLists')
+    new Promise(resolve => setTimeout(resolve, 500)).then(() => {
+      setLists(Array(40).fill('').map((x, idx) => {
+        return {
+          type: 'album',
+          id: idx + 1,
+          name: '默认相册' + idx,
+          count: ~~(Math.random() * 29),
+          url: (idx % 1)
+            ? `http://img4.baixing.net/cda4411639701a0745b0513f968736f8.png_sv1?x=${idx + 1}`
+            : "http://img4.baixing.net/63becd57373449038fcbc3b599aecc8c.jpg_sv1",
+        }
+      }))
+    })
+  }, [])
+
   return [lists, setLists] as const
 }
