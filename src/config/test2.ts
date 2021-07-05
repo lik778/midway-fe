6433a5aabd45af16d7dac2df37d4430b8c0a94e6
明胧 <zhaominglong@baixing.com>
2021-07-05 15:48:12 +0800

    feat:修改host

--- src/config/test2.ts
+++ src/config/test2.ts
@@ -5,7 +5,7 @@ export default {
       host: 'http://172.30.2.14:30263'
     },
     'zhidao-service': {
-      host: 'http://172.30.2.14:30252'
+      host: 'http://172.30.2.14:31252'
     },
     'reporting-service': {
       host: 'http://dev-api.baixing.cn'
@@ -15,7 +15,7 @@ export default {
   haojing: 'http://wulei.baixing.cn',
   haojingDomain: 'baixing.cn',
   hostType: {
-    prefix: ':shopName.shop-test2.baixing.cn',
-    base: 'shop-test2.baixing.cn'
+    prefix: ':shopName.shop-test2.baixing.:domain',
+    base: 'shop-test2.baixing.:domain'
   }
 }
