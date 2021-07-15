90e4b52a09bc34479221e12d6b5265e3dab7ffd3
仿生狮子（一光） <tangnad@qq.com>
2021-07-15 14:59:06 +0800

    fix: 使用 location.origin 替换源站地址（不然测试环境没法测试）

--- src/view-helpers/simple-resouce-reload.ts
+++ src/view-helpers/simple-resouce-reload.ts
@@ -4,38 +4,42 @@ export default `
  * @param {string} 元素的类选择器名称
  */
 function simpleResourceReload(clsName) {
-  const $reload = document.querySelector('.'+clsName)
-  if (!$reload) {
-    throw new Error('[ERR] resources loaded error:', clsName)
+  try {
+    const $reload = document.querySelector('.'+clsName)
+    if (!$reload) {
+      throw new Error('[ERR] resources loaded error:', clsName)
+    }
+    const tagname = $reload.tagName.toLowerCase()
+    const isScript = tagname === 'script'
+    const isStyle = tagname === 'link'
+    const url = isScript ? $reload.src : isStyle ? $reload.href : ''
+  
+    // CDN origin
+    const prefix = 'CDN_PATH'
+    // 资源文件 origin
+    const sourcePrefix = window.location.origin
+  
+    const staticPathReg = new RegExp('(https?:)?'+prefix+'(/.+)*$')
+    const staticPath = url.match(staticPathReg)[2]
+    const sourcePath = sourcePrefix + staticPath
+  
+    const $source = document.createElement(tagname)
+    if (isScript) {
+      $source.setAttribute('src', sourcePath)
+    }
+    if (isStyle) {
+      $source.setAttribute('href', sourcePath)
+      $source.setAttribute('rel', 'stylesheet')
+    }
+    $source.onload = () => simpleResourceReloadLog('[LOAD] resources reloaded: ' + clsName)
+  
+    document.head.appendChild($source)
+  } catch(error) {
+    simpleResourceReloadLog(error.message)
   }
-  const tagname = $reload.tagName.toLowerCase()
-  const isScript = tagname === 'script'
-  const isStyle = tagname === 'link'
-  const url = isScript ? $reload.src : isStyle ? $reload.href : ''
-
-  // CDN origin（same as window.location.origin）
-  const prefix = 'CDN_PATH'
-  // 资源文件 origin
-  const sourcePrefix = 'SOURCE_PATH'
-
-  const staticPathReg = new RegExp('(https?:)?'+prefix+'(/.+)*$')
-  const staticPath = url.match(staticPathReg)[2]
-  const sourcePath = sourcePrefix + staticPath
-
-  const $source = document.createElement(tagname)
-  if (isScript) {
-    $source.setAttribute('src', sourcePath)
-  }
-  if (isStyle) {
-    $source.setAttribute('href', sourcePath)
-    $source.setAttribute('rel', 'stylesheet')
-  }
-  $source.onload = () => simpleResourceReloadLog(clsName)
-
-  document.head.appendChild($source)
 }
 
-function simpleResourceReloadLog(clsName) {
-  console.log('[LOAD] resources reloaded:', clsName)
+function simpleResourceReloadLog(msg) {
+  console.log(msg)
 }
 `
