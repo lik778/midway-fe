a07ee374a47fcecf303bc2fd3f27561a726e22be
仿生狮子（一光） <tangnad@qq.com>
2021-07-21 18:35:40 +0800

    feat: 列表折叠逻辑

--- app/mvip-manager/src/pages/report/message/index.tsx
+++ app/mvip-manager/src/pages/report/message/index.tsx
@@ -1,6 +1,6 @@
-import React, { useCallback, useMemo } from 'react'
+import React, { useState, useCallback, useRef, useMemo } from 'react'
 import { Tabs, Form } from 'antd'
-import { PhoneFilled } from '@ant-design/icons'
+import { PhoneFilled, DownOutlined } from '@ant-design/icons'
 import Loading from '@/components/loading'
 import MainTitle from '@/components/main-title'
 import Query from '../components/search-list'
@@ -17,15 +17,27 @@ import "./index.less"
 const TabPane = Tabs.TabPane
 
 function LeaveMessagePage() {
-  const isMobile = useMemo(() => {
-    var searchParams = new URLSearchParams(window.location.search)
-    return searchParams.get('mobile') === '1'
-  }, [])
+
+  // ********************************************************* states
+
+  const params = useMemo(() => new URLSearchParams(window.location.search), [])
+  const isMobile = useMemo(() => params.get('mobile') === '1', [params])
+  const isPC = useMemo(() => !isMobile, [isMobile])
+
   const [queryForm] = Form.useForm()
   const [lists, loading, refreshList] = useApi<ReportListResData<getLeaveMessageListParams[]> | null, LeaveMessageListData>(null, getLeaveMessageList)
+  const [activeTab, setActiveTab] = useState<string>('1day')
 
-  const $pcPage = useMemo(() => {
-    if (!isMobile) {
+  // ********************************************************* interactions
+
+  const changeTab = (key: string) => {
+    setActiveTab(key)
+  }
+
+  // ********************************************************* renders
+
+  const $pcPage = useCallback(() => {
+    if (isPC) {
       return <>
         <MainTitle title="留资报表" />
         <div className="container">
@@ -43,58 +55,92 @@ function LeaveMessagePage() {
         </div>
       </>
     }
-    return null
-  }, [isMobile])
+  }, [isPC, lists, refreshList])
 
-  const changeTab = (key) => {
-    console.log(key)
-  }
-
-  const $cards = useMemo(() => {
+  const $cards = useCallback(tabKey => {
     const items = lists?.result || []
+    if (!activeTab || tabKey !== activeTab || items.length === 0) {
+      return null
+    }
+    console.log('[INFO] rendered:', tabKey, items)
     return <div className="cards-con">
-      {items.map((item: LeaveMessageListData) => {
-        return <div className="card" key={item.id}>
-          <div className="header">
-            <div className="left">
-              <div className="title">用户刘女士留言</div>
-              <div className="date">2017-10-01 12:00</div>
-            </div>
-            <div className="right">
-              <div className="phone-btn"><PhoneFilled/> 13807823184</div>
-            </div>
-          </div>
-          <div className="message line-2">{item.content}</div>
-          <div className="source">
-            <span>来源【{LeaveMessageChannelMap[item.type] || '未知'}】</span>{item.name}
-          </div>
-        </div>
-      })}
+      {items.map((item: LeaveMessageListData) => <Card item={item} />)}
     </div>
-  }, [lists])
+  }, [lists, activeTab])
+
+  const $mobilePage = useCallback(() => {
+    if (isMobile) {
+      return <Tabs centered defaultActiveKey="1day" onChange={changeTab}>
+        <TabPane tab="今日" key="1day">
+          {loading && <Loading />}
+          {activeTab === '1day' && $cards('1day')}
+        </TabPane>
+        <TabPane tab="近7日" key="7day">
+          {loading && <Loading />}
+          {activeTab === '7day' && $cards('7day')}
+        </TabPane>
+        <TabPane tab="近30日" key="30day">
+          {loading && <Loading />}
+          {activeTab === '30day' && $cards('30day')}
+        </TabPane>
+        <TabPane tab="自定义" key="custom">
+          {loading && <Loading />}
+          {activeTab === 'custom' && $cards('custom')}
+        </TabPane>
+      </Tabs>
+    }
+  }, [isMobile, lists, refreshList, activeTab])
 
   return (
-    <div className={"page-report page-report-keyword" + ' ' + (isMobile ? 'share' : '')}>
-      {!isMobile ? $pcPage : (
-        <>
-          <Tabs defaultActiveKey="1" onChange={changeTab}>
-            <TabPane tab="今日" key="1day">
-              {$cards}
-            </TabPane>
-            <TabPane tab="近7日" key="7day">
-              {$cards}
-            </TabPane>
-            <TabPane tab="近30日" key="30day">
-              {$cards}
-            </TabPane>
-            <TabPane tab="自定义" key="custom">
-              {$cards}
-            </TabPane>
-          </Tabs>
-        </>
-      )}
+    <div className={"page-report page-report-message" + ' ' + (isMobile ? 'share' : '')}>
+      {isPC && $pcPage()}
+      {isMobile && $mobilePage()}
     </div>
   )
+}
+
+
+type CardProps = {
+  item: LeaveMessageListData
+}
+function Card (props: CardProps) {
+  const { item } = props
+  const [shouldFold, setShouldFold] = useState(false)
+  const [fold, setFold] = useState(true)
+
+  const checkEllipses = useCallback((el: HTMLElement | null) => {
+    if (el) {
+      if (el.offsetHeight < el.scrollHeight) {
+        setShouldFold(true)
+      }
+    }
+  }, [setShouldFold])
+
+  return <div className={"card " + (fold ? '' : 'unfold')} key={item.id}>
+    <div className="header">
+      <div className="left">
+        <div className="title">用户刘女士留言</div>
+        <div className="date">2017-10-01 12:00</div>
+      </div>
+      <div className="right">
+        <div className="phone-btn">
+          <PhoneFilled />
+          <a href={`tel:${item.mobile}`}>{item.mobile}</a>
+        </div>
+      </div>
+    </div>
+    <div className="message line-2" ref={el => checkEllipses(el)}>
+      {item.content}
+    </div>
+    <div className="source">
+      <span>来源【{LeaveMessageChannelMap[item.type] || '未知'}】</span>{item.name}
+    </div>
+    {shouldFold && (
+      <div className="fold-btn" onClick={() => setFold(!fold)}>
+        <DownOutlined />
+      </div>
+    )}
+  </div>
 }
 
 LeaveMessagePage.wrappers = ['@/wrappers/path-auth']
