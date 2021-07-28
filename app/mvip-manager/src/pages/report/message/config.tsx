c4a30429c0d69f3e4b0d90675b29aae699cfe61b
仿生狮子（一光） <tangnad@qq.com>
2021-07-28 16:24:46 +0800

    fix: 列表样式问题

--- app/mvip-manager/src/pages/report/message/config.tsx
+++ app/mvip-manager/src/pages/report/message/config.tsx
@@ -69,7 +69,6 @@ export const LeaveMessageSearchListConfig = ({
         title: '来源',
         dataIndex: 'type',
         width: 65,
-        align: 'center',
         key: 'type',
         render: (_: any, r: Item) => LeaveMessageChannelMap[r.sourceType]
       },
@@ -90,9 +89,8 @@ export const LeaveMessageSearchListConfig = ({
         title: '落地页详情',
         dataIndex: 'sourceUrl',
         width: 120,
-        align: 'center',
         key: 'sourceUrl',
-        render: (_: any, r: Item) => <Button type="link"><a href={r.sourceUrl} target="__blank">查看详情</a></Button>
+        render: (_: any, r: Item) => <a href={r.sourceUrl} target="__blank">查看详情</a>
       },
       {
         title: '留咨内容',
