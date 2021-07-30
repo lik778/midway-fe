23b16fd48489d39fe5f1127e0dfe32bb1bedf875
仿生狮子（一光） <tangnad@qq.com>
2021-07-30 11:11:23 +0800

    feat: 脚本，用来快速合并当前分支到测试分支

---
+++ build/chore/utils.js
@@ -0,0 +1,36 @@
+const chalk = require('chalk')
+const exec = require('child_process').exec
+
+const check = async function (fn) {
+  try {
+    const valid = await fn()
+    if (valid instanceof Error) {
+      console.error(chalk.red(String(valid)))
+      process.exit(1)
+    }
+  } catch (error) {
+    console.error(chalk.red(String(error)))
+    process.exit(1)
+  }
+}
+
+const cmd = function (...args) {
+  return new Promise((resolve, reject) => {
+    exec(...args, (error, stdout) => {
+      if (error) return reject(error)
+      if (stdout) return resolve(stdout)
+    })
+  }).catch(error => {
+    console.error(error)
+  })
+}
+
+const line = function () {
+  console && console.log && console.log()
+}
+
+module.exports = {
+  check,
+  cmd,
+  line,
+}
