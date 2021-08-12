5c3e9424524c87b4bdc03b450c5c236ca6cc98f6
明胧 <zhaominglong@baixing.com>
2021-08-12 22:32:06 +0800

    模块管理自查bug

--- app/mvip-manager/src/pages/shop/[id]/module-management/components/content/components/home-swiper/components/pc/index.tsx
+++ app/mvip-manager/src/pages/shop/[id]/module-management/components/content/components/home-swiper/components/pc/index.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState, Ref, forwardRef, useImperativeHandle, useRef, useEffect } from 'react';
 import { useParams } from 'umi';
-import { Button, Radio, RadioChangeEvent, Form } from 'antd'
+import { Spin, Button, Radio, RadioChangeEvent, Form } from 'antd'
 import { QuestionCircleOutlined } from '@ant-design/icons'
 import { ShopInfo } from '@/interfaces/shop';
 import { Detail } from './data'
@@ -22,6 +22,7 @@ const PcSwiper = (props: Props, parentRef: Ref<any>) => {
   const params = useParams<{ id: string }>()
   const { curShopInfo, position, pageModule } = props
   const [swiperType, setSwiperType] = useState<'swiper' | 'product'>(curShopInfo.type === ProductType.B2B ? 'product' : 'swiper')
+  const [upDataLoading, setUpDataLoading] = useState<boolean>(false)
 
   const swiperRef = useRef<{
     handleUpData: (type: 'all') => Promise<void>,
@@ -48,10 +49,12 @@ const PcSwiper = (props: Props, parentRef: Ref<any>) => {
 
   // 保存轮播图需要单独再打个接口
   const handleSubmitNoProduct = async () => {
+    setUpDataLoading(true)
     const res = await setModuleBannerInfoApi(Number(params.id), {
       bannerProduct: false,
       position, pageModule
     })
+    setUpDataLoading(false)
   }
 
   const handleUpData = () => {
@@ -91,9 +94,11 @@ const PcSwiper = (props: Props, parentRef: Ref<any>) => {
     </Form.Item>
     {
       swiperType && <>
-        <CatchComponent visible={swiperType === 'swiper'}>
-          <SwiperComponent ref={swiperRef} position={position} pageModule={pageModule}></SwiperComponent>
-        </CatchComponent>
+        <Spin spinning={upDataLoading}>
+          <CatchComponent visible={swiperType === 'swiper'}>
+            <SwiperComponent ref={swiperRef} position={position} pageModule={pageModule}></SwiperComponent>
+          </CatchComponent>
+        </Spin>
         <CatchComponent visible={swiperType === 'product'}>
           <ProductComponent ref={productRef} setSwiperType={setSwiperType} position={position} pageModule={pageModule}></ProductComponent>
         </CatchComponent>
