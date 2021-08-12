f32181ddd8f756e6406f27c7176ea01af287c03a
李款 <likuan@baixing.com>
2021-08-12 19:37:21 +0800

    feat: b2b,b2c pc端 wap端模板接口对接

--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -64,7 +64,7 @@ export class SiteService {
       // domain = 'zb2c.shop-test.baixing.cn'
       // domain = 'hongtest1.shop-test2.baixing.cn'
 
-      /*后端在test分支，且店铺类型是是模板1，B2C模板，使用这个domain*/
+      /*后端在test分支，且店铺类型是是模板1，B2C模板，使用这个domain*/ 
       domain = 'shop-test.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板2，B2B模板，使用这个domain*/
