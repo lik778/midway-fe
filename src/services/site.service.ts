d524de346c4a21870f89c532ec561d37df2d2e05
likuan <likuan@baixing.com>
2021-08-19 17:21:29 +0800

    style: 自定义模块二限制10个和样式走查

--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -61,11 +61,11 @@ export class SiteService {
       /*后端在test分支，且店铺类型是是模板2，B2B模板，使用这个domain*/
       // domain = 'zmlc2b.shop.baixing.cn'
       // domain = 'hongtest1.shop-test.baixing.cn'
-      // domain = 'zb2c.shop-test.baixing.cn'
+      domain = 'zb2c.shop-test.baixing.cn'
       // domain = 'hongtest1.shop-test2.baixing.cn'
 
       /*后端在test分支，且店铺类型是是模板1，B2C模板，使用这个domain*/
-      domain = 'shop-test.baixing.cn'
+      // domain = 'shop-test.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板2，B2B模板，使用这个domain*/
       // domain = 'shop.baixing.cn'
