51f07d9ee451cf0e014d3733c812f1989eaa20ed
明胧 <zhaominglong@baixing.com>
2021-08-20 18:41:08 +0800

    fix：导航选中问题

--- assets/site-template-2/wap/components/nav/index.js
+++ assets/site-template-2/wap/components/nav/index.js
@@ -1,7 +1,7 @@
 import $ from 'jquery'
 
-export const navModule = function() {
-	$(document).on('ready',function() {
+export const navModule = function () {
+	$(document).on('ready', function () {
 		//$(".nav-item > a").each((index, tab) => {
 		//	const href = $(tab).attr('href')
 		//	if (href.replace(/\/$/, '').endsWith(window.location.pathname.replace(/[n|p]\-?\d*.*\/?$|\/$/, function(result) {
@@ -14,10 +14,10 @@ export const navModule = function() {
 		const windowPath = window.location.pathname
 
 		$(".nav a").each((index, tab) => {
-			const h = $(tab).attr('href')
-			const n = h.slice(-2,-1); //提取导航里的尾部，展示n,p
+			const h = $(tab).attr('href').replace(/#{0,}/g, '')
+			const n = h.slice(-2, -1); //提取导航里的尾部，展示n,p
 			const t = windowPath.slice(1, 2); //提取当前url的尾部，展示n,p
-			if(h==windowHref || t==n || windowPath.indexOf(h)!==-1){
+			if (h && (h == windowHref || t == n || windowPath.indexOf(h) !== -1)) {
 				$(tab).addClass('nav-active')
 				//siblings():所有同级a 元素去掉该class。
 				$(tab).parent().siblings().find('a').removeClass('nav-active')
