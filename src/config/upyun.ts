4174d8c38a758583c704d97b783199bc22cefddd
仿生狮子（一光） <tangnad@qq.com>
2021-08-30 17:08:03 +0800

    feat: 更改又拍云上传视频时的回调地址（分为线上线下）

--- src/config/upyun.ts
+++ src/config/upyun.ts
@@ -1,19 +1,15 @@
 export const upyunImgConfig = {
   "policy": {
-    // bucket 回调的值
     "bucket": "bximg",
     "save-key": "/{filemd5}{.suffix}",
     "content-secret": "original",
     "content-length-range": "0,10485760",
     "x-gmkerl-type": "fix_max",
     "x-gmkerl-value": "3840",
-    // 2B的又拍改了代码导致不认boolean true了，先用字符串的"true"
     "x-gmkerl-noicc": "true",
     "x-gmkerl-quality": "100",
     "x-gmkerl-rotate": "auto",
-    // 2B的又拍改了代码导致不认boolean true了，先用字符串的"true"
     "x-gmkerl-exif-switch": "true",
-    // bucket 回调的值
     "notify-url": "http://callback.img.baixing.net/upyun.php?bucket=bximg"
   },
   "form_api_secret": "icjvaWAQFMBkRfxuovi3xy3w3ik=",
@@ -22,18 +18,28 @@ export const upyunImgConfig = {
   "imageHost": undefined
 }
 
-/* @see https://help.upyun.com/knowledge-base/av/#e5b8b8e794a8e58f82e695b0 */
-export const upyunVideoConfig = {
-  host: 'http://bxmedia.baixing.net',
-  form_api_secret: "9sMCIBryAo8INghVqfOQXtsvTNI=",
-  operateHost: 'http://p0.api.upyun.com',
-  policy: {
-    "bucket": "bxmedia",
-    "save-key": "/{filemd5}{.suffix}",
-    "type": "nbhd",
-    "advote": "/s/720p(16:9)/r/25/sm/false/ar/22050",
-    /* TODO for test only */ 
-    "notify-url": "https://enb6hk1stgczkkc.m.pipedream.net",
-    "ext-param": "type=mvip-encode"
-  },
+const env = {
+  get upyunVideoConfig () {
+    const isProd = process.env.NODE_ENV === 'production'
+    const notifyURL = isProd
+      ? 'http://api.baixing.com.cn:80/api/midway/internal/material/materialReapply'
+      : 'http://172.30.2.14:30263/api/midway/internal/material/materialReapply'
+    console.log('notifyURL', notifyURL)
+    return {
+      host: 'http://bxmedia.baixing.net',
+      form_api_secret: "9sMCIBryAo8INghVqfOQXtsvTNI=",
+      operateHost: 'http://p0.api.upyun.com',
+      policy: {
+        "bucket": "bxmedia",
+        "save-key": "/{filemd5}{.suffix}",
+        "type": "nbhd",
+        "advote": "/s/720p(16:9)/r/25/sm/false/ar/22050",
+        "notify-url": notifyURL,
+        "ext-param": "source=msshop&s=720p&type=mvip-encode"
+      }
+    }
+  }
 }
+
+/* @see https://help.upyun.com/knowledge-base/av/#e5b8b8e794a8e58f82e695b0 */
+export const upyunVideoConfig = env.upyunVideoConfig
