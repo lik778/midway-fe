ec7334705f5b37d831b2171b8c4677509d2e2400
likuan <likuan@baixing.com>
2021-09-01 11:08:20 +0800

    style: 店铺样式迭代+swiper代码优化

--- assets/site-template-3/wap/components/drawer-nav/index.js
+++ assets/site-template-3/wap/components/drawer-nav/index.js
@@ -1,49 +1,56 @@
-import $ from 'jquery'
+import $ from 'jquery';
 
-export const initNav = function () {
-  const navSelectIcon = $('.nav-select-icon')
-  const drawerNavBox = $('.drawer-nav-box')
-  const drawerNavMask = $('.drawer-nav-mask')
-  const body = $('body')
-  const html = $('html')
-  body.css({ transition: "all 0.5s" });
+export const initNav = function() {
+  const navSelectIcon = $('.nav-select-icon');
+  const drawerNavBox = $('.drawer-nav-box');
+  const drawerNavMask = $('.drawer-nav-mask');
+  const body = $('body');
+  const html = $('html');
+  body.css({ transition: 'all 0.5s' });
 
-  navSelectIcon.on('click', function () {
-    navSelectIcon.addClass('active')
-    drawerNavBox.addClass('active')
-    drawerNavMask.addClass('active')
-    body.css({ transform: 'translateX(-180px)' });
-    html.css({ width: '100vw', height: '100vh', overflow: 'hidden' })
-  })
-  drawerNavMask.on('click', function () {
-    navSelectIcon.removeClass('active')
-    drawerNavBox.removeClass('active')
-    drawerNavMask.removeClass('active')
+  navSelectIcon.on('click', function() {
+    navSelectIcon.addClass('active');
+    drawerNavBox.addClass('active');
+    drawerNavMask.addClass('active');
+    body.css({ transform: 'translateX(180px)' });
+    html.css({ width: '100vw', height: '100vh', overflow: 'hidden' });
+  });
+  drawerNavMask.on('click', function() {
+    navSelectIcon.removeClass('active');
+    drawerNavBox.removeClass('active');
+    drawerNavMask.removeClass('active');
     body.css({ transform: '' });
-    html.css({ width: 'auto', height: 'auto', overflow: 'visible' })
-  })
+    html.css({ width: 'auto', height: 'auto', overflow: 'visible' });
+  });
 
   const imgSrc = {
     0: '//file.baixing.net/202105/ebad606fef0001ec73d15fc4e51921c4.png',
     1: '//file.baixing.net/202105/609ea6ca5dc441be4501410db0509cfe.png',
     2: '//file.baixing.net/202105/1cdccfd92f8c5e86ae808e362cf2cd80.png',
     3: '//file.baixing.net/202105/dac0b607fef01e3a1ebd03a440a1922a.png',
-  }
-  $(document).on('ready', function () {
-    const windowPath = window.location.pathname.indexOf('-') !== -1 ? window.location.pathname.split('-')[0] : window.location.pathname
-    const windowHref = window.location.origin + windowPath
-    let flag = 0
-    $(".nav-item").each((index, tab) => {
-      if (flag === 1) return
+  };
+  $(document).on('ready', function() {
+    const windowPath =
+      window.location.pathname.indexOf('-') !== -1
+        ? window.location.pathname.split('-')[0]
+        : window.location.pathname;
+    const windowHref = window.location.origin + windowPath;
+    let flag = 0;
+    $('.nav-item').each((index, tab) => {
+      if (flag === 1) return;
       const tabHref = $(tab).attr('href');
-      if (tabHref === windowHref || tabHref.indexOf(windowHref) !== -1 || tabHref.indexOf(windowPath) !== -1) {
-        flag = 1
-        $(tab).addClass('active')
-        const imgList = $(tab).find('img')
+      if (
+        tabHref === windowHref ||
+        tabHref.indexOf(windowHref) !== -1 ||
+        tabHref.indexOf(windowPath) !== -1
+      ) {
+        flag = 1;
+        $(tab).addClass('active');
+        const imgList = $(tab).find('img');
         if (imgList && imgList.length > 0) {
-          $(imgList[0]).attr('src', imgSrc[index])
+          $(imgList[0]).attr('src', imgSrc[index]);
         }
       }
-    })
-  })
-}
+    });
+  });
+};
