4ed8baa5392da7a08092f4b6c8eabcfcf0b8045b
lionad-trident <tangnad@qq.com>
2021-09-08 22:52:33 +0800

    fix: 临时兼容模板脏数据

--- src/services/site.service.ts
+++ src/services/site.service.ts
@@ -64,11 +64,11 @@ export class SiteService {
       // domain = 'zb2c.shop-test.baixing.cn'
 
       /*后端在test分支，且店铺类型是是模板1，B2C模板，使用这个domain*/
-      domain = 'shop-test.baixing.cn'
+      // domain = 'shop-test.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板2，B2B模板，使用这个domain*/
       // domain = 'shop.baixing.cn'
-      // domain = 'agui.shop.baixing.cn'
+      domain = 'agui.shop.baixing.cn'
 
       /*后端在dev分支，且店铺类型是是模板1，B2C模板，使用这个domain*/
       // domain = 'shop.baixing.cn'
