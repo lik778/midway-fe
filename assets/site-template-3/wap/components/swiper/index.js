dc7d3c2455c8024c53b83ac225ed77dc2c27ab0f
明胧 <zhaominglong@baixing.com>
2021-09-13 13:42:37 +0800

    fix:wap b2c video

--- assets/site-template-3/wap/components/swiper/index.js
+++ assets/site-template-3/wap/components/swiper/index.js
@@ -38,48 +38,51 @@ const swiper = new Swiper('.swiper-container', {
       } else {
         this.autoplay.start()
       }
+    },
+    init: function () {
+      const _this = this
+      const timer = setInterval(function () {
+        /* 轮播图 */
+        if (_this.$el) {
+          clearInterval(timer)
+          $swiper = _this.$el.length ? _this.$el[0] : _this.$el
+          $slides = $swiper.querySelectorAll('.swiper-slide')
+          const getCurSlide = () => $slides[swiper.activeIndex]
+          const getVideo = () => {
+            const $curSlide = getCurSlide()
+            return [
+              $curSlide.querySelector('video'),
+              $curSlide.querySelector('.video-cover')
+            ]
+          }
+          $swiper.addEventListener('click', function () {
+            $swiper.classList.toggle('fullscreen')
+            const [$video, $cover] = getVideo()
+            if ($video) {
+              const isPaused = $video.paused
+              if (isPaused) {
+                $cover && $cover.remove()
+                $video.play()
+              } else {
+                $video.pause()
+              }
+            }
+          })
+          /* 视频播放时暂停轮播 */
+          $bannerVideos.map($video => {
+            $video.onplay = function () {
+              swiper.autoplay.stop()
+              window._cbs && window._cbs.pauseAll()
+            }
+            $video.onpause = function () {
+              if (!$swiper.classList.contains('fullscreen')) {
+                swiper.autoplay.start()
+                window._cbs && window._cbs.resumeAll()
+              }
+            }
+          })
+        }
+      }, 10)
     }
   }
 })
-
-/* 轮播图 */
-if (swiper.$el) {
-  $swiper = swiper.$el.length ? swiper.$el[0] : swiper.$el
-  $slides = $swiper.querySelectorAll('.swiper-slide')
-  const getCurSlide = () => $slides[swiper.activeIndex]
-  const getVideo = () => {
-    const $curSlide = getCurSlide()
-    return [
-      $curSlide.querySelector('video'),
-      $curSlide.querySelector('.video-cover')
-    ]
-  }
-  $swiper.addEventListener('click', function () {
-    $swiper.classList.toggle('fullscreen')
-    const [$video, $cover] = getVideo()
-    if ($video) {
-      const isPaused = $video.paused
-      if (isPaused) {
-        $cover && $cover.remove()
-        $video.play()
-      } else {
-        $video.pause()
-      }
-    }
-  })
-}
-
-/* 视频播放时暂停轮播 */
-$bannerVideos.map($video => {
-  $video.onplay = function () {
-    swiper.autoplay.stop()
-    window._cbs && window._cbs.pauseAll()
-  }
-  $video.onpause = function () {
-    if (!$swiper.classList.contains('fullscreen')) {
-      swiper.autoplay.start()
-      window._cbs && window._cbs.resumeAll()
-    }
-  }
-})
-
