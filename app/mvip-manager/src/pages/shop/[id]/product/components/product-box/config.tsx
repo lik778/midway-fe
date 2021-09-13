6517d0b43bd1e2c3c1b9c18dcf184e5eabce17e8
明胧 <zhaominglong@baixing.com>
2021-09-13 15:45:50 +0800

    fix:视频封面图比例问题

--- app/mvip-manager/src/pages/shop/[id]/product/components/product-box/config.tsx
+++ app/mvip-manager/src/pages/shop/[id]/product/components/product-box/config.tsx
@@ -16,7 +16,7 @@ export const productForm: FormConfig = {
       formItemWidth: 150, label: '用于封面', name: 'media', type: FormType.ImgUpload, required: false, maxLength: 1, images: [{ uploadType: 2, showVideo: true, text: '用于封面', name: 'media', maxSize: 3, cropProps: { aspectRatio: 300 / 200 }, aspectRatio: 300 / 200 }],
       tip: <>
         <p className={styles['media-tip']}>图片格式：jpg、jpeg、png，大小不超过3M，图片比例3：2，尺寸需大于300*200</p>
-        <p className={styles['media-tip']}>视频建议尺寸：16:9，大小100M以内，视频时长最短3S，最长60S'</p>
+        <p className={styles['media-tip']}>视频建议尺寸：16:9，大小100M以内，视频时长最短3S，最长60S</p>
       </>
     },
     {
