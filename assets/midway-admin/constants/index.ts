ca00981516313ca7602b4496d32f1f7bacc93695
明胧 <zhaominglong@baixing.com>
2021-11-10 13:37:51 +0800

    fix:优选推后台bug

---
+++ assets/midway-admin/constants/index.ts
@@ -0,0 +1,8 @@
+export const COOKIE_TOKEN_KEY = '__t'
+export const COOKIE_USER_KEY = '__u'
+export const COOKIE_HASH_KEY = '__c'
+export const HAO_JING_HOST = '//www.baixing.com'
+// 打点的关键字 track
+export const BXMAINSITE = 'bxmainsite'
+
+
