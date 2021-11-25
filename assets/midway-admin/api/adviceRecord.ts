5bf9da699676ec69b0a6ebf4ce4907d150dde254
明胧 <zhaominglong@baixing.com>
2021-11-25 14:28:45 +0800

    feat:意见收集

--- assets/midway-admin/api/adviceRecord.ts
+++ assets/midway-admin/api/adviceRecord.ts
@@ -1,13 +1,27 @@
 import { postApi, getApi } from '../api/base'
 import { PageParams } from '../interfaces/api';
 import { Advicerespone } from '../interfaces/adviceRecord';
+import { message } from 'antd'
 
 export const getAdviceList = (params: PageParams) => {
   return postApi<Advicerespone[]>('/api/midway/manager/feedback/listing', params)
 }
 
-export const getDownload = () => {
+export const downloadFeedbacks = () => {
   return getApi('/api/midway/manager/feedback/download', {}, {
     baseURL: '/management/api/download-file', responseType: 'blob'
   });
+}
+
+export const downloadFC = async () => {
+  const data = await downloadFeedbacks()
+  try {
+    const a = document.createElement('a');
+    const url = window.URL.createObjectURL(new Blob([data as any], { type: "application/octet-stream" }));
+    a.href = url;
+    a.download = '意见收集.csv';
+    a.click();
+  } catch {
+    message.error('下载失败');
+  }
 }
