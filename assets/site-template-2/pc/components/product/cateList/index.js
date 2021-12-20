460ec6346ea7a02046abd88f9cb38d8c598aa9ab
赵明龙 <zhaominglong@baixing.com>
2021-12-20 10:25:04 +0800

    【SEO】—收录问题—店铺页面HTML改造

--- assets/site-template-2/pc/components/product/cateList/index.js
+++ assets/site-template-2/pc/components/product/cateList/index.js
@@ -2,7 +2,9 @@ import $ from 'jquery'
 
 export const initCateList = () => {
   $(document).on('ready',()=>{
-    const windowPath = window.location.pathname;
+    // 这里的匹配有问题  /p-1  能匹配到 /p-11 /p-111  不过一个用户的类目基本上连续  不怎么可能出现那种情况 不用管咯
+    const windowPath = window.location.pathname.replace(/\/([\s\S]{0,})l(-?)([\s\S]{0,}).html/, '/$1$2$3');
+    if (windowPath.indexOf('-') === -1) return
     $(".products-categories a").each((index, tab) => {
       const tabHref = $(tab).attr('href')
       if (tabHref.indexOf(windowPath) != -1) {
