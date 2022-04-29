d16ad737de0957882e3be2efd02354d04acafab3
guojiaxin@qq.com <guojiaxin@baixing.com>
2022-04-29 17:11:44 +0800

    bug

--- assets/common/wap/customer-service/index.js
+++ assets/common/wap/customer-service/index.js
@@ -1,7 +1,7 @@
 import './index.styl'
 
-const entranceBtn = $('#entrance')
-entranceBtn.on('click',()=>{
-	const {position} = window.extraContactFormData
-    window.location.href = `safeguard?position=${position}`
-})
+// const entranceBtn = $('#entrance')
+// entranceBtn.on('click',()=>{
+// 	const {position} = window.extraContactFormData
+//     window.location.href = `safeguard?position=${position}`
+// })
