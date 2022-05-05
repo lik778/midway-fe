ac37bffc918fe3ef179066a3080dc127df510a41
guojiaxin@qq.com <guojiaxin@baixing.com>
2022-05-05 15:21:48 +0800

    样式和wap入口

--- assets/common/wap/customer-service/index.js
+++ assets/common/wap/customer-service/index.js
@@ -1,7 +1,16 @@
-import './index.styl'
+import $ from 'jquery'
 
-// const entranceBtn = $('#entrance')
-// entranceBtn.on('click',()=>{
-// 	const {position} = window.extraContactFormData
-//     window.location.href = `safeguard?position=${position}`
-// })
+export const jumpAndClick = () => {
+    $(document).on('ready', function () {
+        const serviceContent = $('.customer-service .service-content')
+        serviceContent.on('click',()=>{
+            console.log(serviceContent);
+            serviceContent.toggleClass('service-click')
+        })
+        const entranceBtn = $('.customer-service .bax-btn')
+        entranceBtn.on('click',()=>{
+            const {position} = window.extraContactFormData
+            window.location.href = `safeguard?position=${position}`
+        })
+    })
+}    
