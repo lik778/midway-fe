c1a3fa12fd4fd93d0a7096710785bb12dc8011e5
guojiaxin@qq.com <guojiaxin@baixing.com>
2022-05-09 11:00:19 +0800

    提交

--- assets/common/pc/customer-service/index.js
+++ assets/common/pc/customer-service/index.js
@@ -122,5 +122,13 @@ baxFeedbackCopyBth.on('click', () => {
 const entranceBtn = $('#entrance')
 entranceBtn.on('click',()=>{
 	const {position} = window.extraContactFormData
-    window.location.href = `safeguard?position=${position}`
+	const href = window.location.href
+	const id = /(?<=-)\d{4}(?=.html)/.exec(href)?/(?<=-)\d{4}(?=.html)/.exec(href)[0]:''
+	if(id){
+		window.location.href = `safeguard?position=${position}&id=${id}`
+	}else{
+		window.location.href = `safeguard?position=${position}`
+	}
 })
+
+
--- assets/common/wap/customer-service/index.js
+++ assets/common/wap/customer-service/index.js
@@ -10,6 +10,11 @@ export const jumpAndClick = () => {
         const entranceBtn = $('.customer-service .bax-btn')
         entranceBtn.on('click',()=>{
             const {position} = window.extraContactFormData
+            const href = window.location.href
+            const id = /(?<=-)\d{4}(?=.html)/.exec(drawer-nav-box)?/(?<=-)\d{4}(?=.html)/.exec(href)[0]:''
+            if(id){
+                window.location.href = `safeguard?position=${position}&id=${id}`
+            }
             window.location.href = `safeguard?position=${position}`
         })
     })
