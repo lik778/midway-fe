ca743c59035a4e3bdf1df39a9cf5f579116d1657
dupengfei <18301930595@163.com>
2022-05-24 15:38:53 +0800

    修复site-4-wap的导航组建

--- assets/common/dynamic-nav.js
+++ assets/common/dynamic-nav.js
@@ -21,31 +21,33 @@ export const initNav = function ($navs) {
     return item.getAttribute('navLink')
   })
 
+	const index = urls.findIndex(item => item === href)
+	_setTargetElActive(index)
 
-	const index = urls.findIndex(item => {
-    if (item === href) return true
-    if (item.endsWith('/') && !href.endsWith('/')) {
-      return item === `${href}/`
-    }
-    if (!item.endsWith('/') && href.endsWith('/')) {
-      return `${item}/` === href
-    }
-  })
+	// const index = urls.findIndex(item => {
+  //   if (item === href) return true
+  //   if (item.endsWith('/') && !href.endsWith('/')) {
+  //     return item === `${href}/`
+  //   }
+  //   if (!item.endsWith('/') && href.endsWith('/')) {
+  //     return `${item}/` === href
+  //   }
+  // })
 
-	if (index !== -1) {
-		_setTargetElActive(index)
-    return
-  }
+	// if (index !== -1) {
+	// 	_setTargetElActive(index)
+  //   return
+  // }
 
-  let baseUrl = urls[0]
-  if (href.startsWith(`${baseUrl}p`)) {
-    const index = urls.findIndex(item => item === `${baseUrl}pl.html`)
-		_setTargetElActive(index)
-    return
-  }
-  if (href.startsWith(`${baseUrl}n`)) {
-    const index = urls.findIndex(item => item === `${baseUrl}nl.html`)
-		_setTargetElActive(index)
-    return
-  }
+  // let baseUrl = urls[0]
+  // if (href.startsWith(`${baseUrl}p`)) {
+  //   const index = urls.findIndex(item => item === `${baseUrl}pl.html`)
+	// 	_setTargetElActive(index)
+  //   return
+  // }
+  // if (href.startsWith(`${baseUrl}n`)) {
+  //   const index = urls.findIndex(item => item === `${baseUrl}nl.html`)
+	// 	_setTargetElActive(index)
+  //   return
+  // }
 }
