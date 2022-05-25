505eaf2358a2f331ad35aa3ca5ce18f720bc282e
dupengfei <18301930595@163.com>
2022-05-25 11:56:29 +0800

    添加日志打点0.3

--- assets/site-template-4/wap/home/index.js
+++ assets/site-template-4/wap/home/index.js
@@ -9,11 +9,15 @@ import { initProductListHot } from '../components/product-list-hot'
 import { initSem } from '../../../common/wap/contact-form-sem'
 
 initAboutus()
+console.log("__log initAboutus");
 leaveLeads()
+console.log("__log leaveLeads");
 initCompanyInfo()
+console.log("__log initCompanyInfo");
 initProductListHot()
+console.log("__log initProductListHot");
 
-// sem下禁止用户二跳 
+// sem下禁止用户二跳
 $(document).on("ready", function () {
   // sem需要禁止部分内容二跳
   // tapd:https://www.tapd.cn/20095111/prong/stories/view/1120095111001038653
@@ -28,8 +32,8 @@ $(document).on("ready", function () {
       gotoOtherPageA: $('.products a')
     })
   }
-  
-  if (isSem === '2') {
+
+	if (isSem === '2') {
     initSem({
       account: isAccount,
       sem: isSem,
